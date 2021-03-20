a3c2652a4c2eea70add6b0d5ea8a0865f29345e8
xgambitox <xgambitox@gmail.com>
2021-03-20 16:08:01 -0600

    chore: add path mappings

---
+++ craco.config.js
@@ -0,0 +1,14 @@
+const CracoAlias = require('craco-alias');
+
+module.exports = {
+  plugins: [
+    {
+      plugin: CracoAlias,
+      options: {
+        source: 'tsconfig',
+        baseUrl: './src',
+        tsConfigPath: './tsconfig.paths.json',
+      },
+    },
+  ],
+};
