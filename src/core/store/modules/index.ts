fc8d4d135bc22a17699d3875a20f034916f9eee4
xgambitox <xgambitox@gmail.com>
2021-03-21 10:54:59 -0600

    chore: add router

--- src/core/store/modules/index.ts
+++ src/core/store/modules/index.ts
@@ -1,13 +1,15 @@
 import { combineReducers, Reducer } from '@reduxjs/toolkit';
 
 import appReducer, { initApp } from './app';
+import routeReducer, { changeRoute } from './route';
 import themeReducer, { changeTheme } from './theme';
 import { RootState } from '@types';
 
 const rootReducer: Reducer<RootState> = combineReducers({
   app: appReducer,
+  route: routeReducer,
   theme: themeReducer,
 });
 
 export default rootReducer;
-export { initApp, changeTheme };
+export { initApp, changeRoute, changeTheme };
