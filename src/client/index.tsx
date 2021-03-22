2aad77c2282e7fc84bedbf2773f78cb4ac54f606
xgambitox <xgambitox@gmail.com>
2021-03-22 11:27:50 -0600

    chore: add global style

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,8 +1,16 @@
 import { Provider } from 'react-redux';
+import { createGlobalStyle } from 'styled-components';
 
 import { getStore } from '@frameworks/redux';
 import { Routes } from '@routes';
 import { Themable } from '@containers';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+  }
+`;
 
 const store = getStore();
 
@@ -10,6 +18,7 @@ export const App = () => {
   return (
     <Provider store={store}>
       <Themable>
+        <GlobalStyle />
         <Routes />
       </Themable>
     </Provider>
