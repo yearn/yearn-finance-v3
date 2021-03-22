4d922909b3996a0e69d52d0ebddf2263d1c9c6f4
xgambitox <xgambitox@gmail.com>
2021-03-22 15:07:06 -0600

    chore: add ethers

--- src/config/env/index.ts
+++ src/config/env/index.ts
@@ -7,5 +7,11 @@ export const getEnv = memoize(
     ENV: process.env.NODE_ENV,
     ETHEREUM_NETWORK: (process.env.NEXT_PUBLIC_ETHEREUM_NETWORK ||
       'mainnet') as EthereumNetwork,
+    INFURA_PROJECT_ID: process.env.INFURA_PROJECT_ID,
+    ETHERSCAN_API_KEY: process.env.ETHERSCAN_API_KEY,
+    ALCHEMY_API_KEY: process.env.ALCHEMY_API_KEY,
+    BLOCKNATIVE_KEY: process.env.BLOCKNATIVE_KEY,
+    FORTMATIC_KEY: process.env.FORTMATIC_KEY,
+    PORTIS_KEY: process.env.PORTIS_KEY,
   })
 );
