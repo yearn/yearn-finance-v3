7e396d4a376c39ef4338657737b2904b7a349f87
xgambitox <xgambitox@gmail.com>
2021-03-22 22:13:36 -0600

    chore: fix env variables

--- src/config/env/index.ts
+++ src/config/env/index.ts
@@ -5,13 +5,13 @@ import { Env, EthereumNetwork } from '@types';
 export const getEnv = memoize(
   (): Env => ({
     ENV: process.env.NODE_ENV,
-    ETHEREUM_NETWORK: (process.env.NEXT_PUBLIC_ETHEREUM_NETWORK ||
+    ETHEREUM_NETWORK: (process.env.REACT_APP_ETHEREUM_NETWORK ||
       'mainnet') as EthereumNetwork,
-    INFURA_PROJECT_ID: process.env.INFURA_PROJECT_ID,
-    ETHERSCAN_API_KEY: process.env.ETHERSCAN_API_KEY,
-    ALCHEMY_API_KEY: process.env.ALCHEMY_API_KEY,
-    BLOCKNATIVE_KEY: process.env.BLOCKNATIVE_KEY,
-    FORTMATIC_KEY: process.env.FORTMATIC_KEY,
-    PORTIS_KEY: process.env.PORTIS_KEY,
+    INFURA_PROJECT_ID: process.env.REACT_APP_INFURA_PROJECT_ID,
+    ETHERSCAN_API_KEY: process.env.REACT_APP_ETHERSCAN_API_KEY,
+    ALCHEMY_API_KEY: process.env.REACT_APP_ALCHEMY_API_KEY,
+    BLOCKNATIVE_KEY: process.env.REACT_APP_BLOCKNATIVE_KEY,
+    FORTMATIC_KEY: process.env.REACT_APP_FORTMATIC_KEY,
+    PORTIS_KEY: process.env.REACT_APP_PORTIS_KEY,
   })
 );
