f9b4beb2627c96cee44ac175c5e6a146a994c348
gotzenx <gotzenlenek@gmail.com>
2021-03-29 15:01:31 -0300

    chore: deleted comments

--- src/core/store/modules/vaults/vaults.actions.ts
+++ src/core/store/modules/vaults/vaults.actions.ts
@@ -11,15 +11,3 @@ export const getVaults = createAsyncThunk<
   const supportedVaults = await getSupportedVaults.execute();
   return { supportedVaults };
 });
-
-// export namespace VaultsActions {
-//   export const getVaults = createAsyncThunk<
-//     any[],
-//     string | undefined,
-//     ThunkAPI
-//   >('vaults/getVaults', async (_arg, { extra }) => {
-//     const { getSupportedVaults } = extra.services;
-//     const supportedVaults = await getSupportedVaults.execute();
-//     return supportedVaults;
-//   });
-// }
