8504de250845e68d624f61ee2e6ea1ddb23e7c1c
xgmolinax <xgmolinax@gmail.com>
2021-03-29 15:52:02 -0600

    feat: sdk initial implementation

--- src/core/types/Service.ts
+++ src/core/types/Service.ts
@@ -1,12 +1,7 @@
-import { EthereumAddress } from '@types';
+import { VaultData } from '@types';
 
 export interface Service<T1, T2> {
   execute(props?: T1): T2 | Promise<T2>;
 }
 
-export interface GetSupportedVaultsServiceResult {
-  vaultAddress: EthereumAddress;
-  tokenAddress: EthereumAddress;
-}
-
-export interface GetSupportedVaultsService extends Service<void, GetSupportedVaultsServiceResult[]> {}
+export interface GetSupportedVaultsService extends Service<void, VaultData[]> {}
