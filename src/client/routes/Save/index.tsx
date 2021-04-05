6bb9469c261114e98ce8dd4c30c368553852baa6
xgmolinax <xgmolinax@gmail.com>
2021-04-05 15:42:41 -0600

    feat: add vault icon

--- src/client/routes/Save/index.tsx
+++ src/client/routes/Save/index.tsx
@@ -75,7 +75,7 @@ export const Save = () => {
           Component={AssetCard}
           items={vaults.map((vault) => ({
             key: vault.address,
-            icon: `https://raw.githack.com/iearn-finance/yearn-assets/master/icons/tokens/0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE/logo-128.png`,
+            icon: `https://raw.githack.com/iearn-finance/yearn-assets/master/icons/tokens/${vault.token.address}/logo-128.png`,
             name: vault.name,
             balance: formatAmount(weiToUnits(vault.vaultBalance, 18), 2),
             earning: '',
