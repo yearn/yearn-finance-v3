030d8297edbdd5c5cd2082609ca7af30dbc9a99d
turtlemoji <turtlemoji@protonmail.com>
2021-04-05 22:23:04 -0300

    refactor: splashscreen

--- src/client/routes/Landing/index.tsx
+++ src/client/routes/Landing/index.tsx
@@ -1,26 +1,59 @@
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
 import { useAppTranslation } from '@hooks';
 import { Box, Button, Text } from '@components/common';
+
 import { YearnSplash } from '@assets/images';
+import YearnSplashVideo from '@assets/videos/yearn-splash-video.mp4';
+
+const LandingView = styled.div`
+  display: flex;
+  position: relative;
+  flex: 1;
+`;
+
+const StyledVideo = styled.video`
+  object-fit: cover;
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  background-image: url(${YearnSplash});
+  background-size: cover;
+  background-position: center;
+`;
+
+const SplashContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+  position: relative;
+`;
 
 export const Landing = () => {
   const { t } = useAppTranslation('common');
 
   return (
-    <Box center flex={1} backgroundImage={`url(${YearnSplash})`} backgroundSize="cover" backgroundPosition="center">
-      <Text fontSize={26} mt={80} textAlign="center">
-        THE
-      </Text>
-      <Text fontSize={117} textAlign="center">
-        {t('title')}
-      </Text>
-      <Text fontSize={18} mt={17} mb={40} width={418} lineHeight="31px" textAlign="center">
-        {t('subtitle')}
-      </Text>
-      <Button>
-        <Link to="/Save">Go to App</Link>
-      </Button>
-    </Box>
+    <LandingView>
+      <StyledVideo src={YearnSplashVideo} autoPlay loop playsInline muted />
+      <SplashContent>
+        <Text fontSize={26} mt={80} textAlign="center">
+          THE
+        </Text>
+        <Text fontSize={117} textAlign="center">
+          {t('title')}
+        </Text>
+        <Text fontSize={18} mt={17} mb={40} width={418} lineHeight="31px" textAlign="center">
+          {t('subtitle')}
+        </Text>
+        <Button>
+          <Link to="/Save">Go to App</Link>
+        </Button>
+      </SplashContent>
+    </LandingView>
   );
 };
