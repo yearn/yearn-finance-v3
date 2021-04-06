12c242d0208115c593e08e50e112bd8ba9fe108b
xgmolinax <xgmolinax@gmail.com>
2021-04-05 18:14:39 -0600

    feat: add landing background

---
+++ src/client/assets/images/index.ts
@@ -0,0 +1,5 @@
+import YearnLogoFull from './yearn-logo-full.svg';
+import YearnLogo from './yearn-logo.svg';
+import YearnSplash from './yearn-splash.svg';
+
+export { YearnLogoFull, YearnLogo, YearnSplash };
--- src/client/routes/Landing/index.tsx
+++ src/client/routes/Landing/index.tsx
@@ -1,16 +1,25 @@
 import { Link } from 'react-router-dom';
-import { useAppTranslation } from '@hooks';
 
-import { Box, Button } from '@components/common';
+import { useAppTranslation } from '@hooks';
+import { Box, Button, Text } from '@components/common';
+import { YearnSplash } from '@assets/images';
 
 export const Landing = () => {
   const { t } = useAppTranslation('common');
 
   return (
-    <Box center flex={1}>
-      {t('title')}
+    <Box center flex={1} backgroundImage={`url(${YearnSplash})`} backgroundSize="cover" backgroundPosition="center">
+      <Text fontSize={26} mt={80} textAlign="center">
+        THE
+      </Text>
+      <Text fontSize={117} textAlign="center">
+        {t('title')}
+      </Text>
+      <Text fontSize={18} mt={17} mb={40} width={418} lineHeight="31px" textAlign="center">
+        {t('subtitle')}
+      </Text>
       <Button>
-        <Link to="/Save">Go to save</Link>
+        <Link to="/Save">Go to App</Link>
       </Button>
     </Box>
   );
