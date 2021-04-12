f9fc36e98636f4f7492669f9d80e6ade410b07b7
gotzenx <gotzenlenek@gmail.com>
2021-04-12 11:53:04 -0300

    refactor: app actions

--- src/core/store/modules/app/app.actions.ts
+++ src/core/store/modules/app/app.actions.ts
@@ -3,12 +3,14 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import { ThunkAPI } from '@frameworks/redux';
 import { WalletActions, TokensActions } from '@store';
 
+export const initApp = createAsyncThunk<void, void, ThunkAPI>('app/initApp', async (_arg, { dispatch, getState }) => {
+  const { wallet } = getState();
+  if (wallet.name) {
+    dispatch(WalletActions.walletSelect(wallet.name));
+  }
+  dispatch(TokensActions.getTokens());
+});
+
 export const AppActions = {
-  initApp: createAsyncThunk<void, void, ThunkAPI>('app/initApp', async (_arg, { dispatch, getState }) => {
-    const { wallet } = getState();
-    if (wallet.name) {
-      dispatch(WalletActions.walletSelect(wallet.name));
-    }
-    dispatch(TokensActions.getTokens());
-  }),
+  initApp,
 };
