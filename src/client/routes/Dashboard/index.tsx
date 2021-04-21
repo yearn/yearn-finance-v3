f8baa85d3a05f93ceb37b9d47641ff186735c118
turtlemoji <78362532+turtlemoji@users.noreply.github.com>
2021-04-21 18:21:11 -0300

    feat: basic routes and nav (#16)

---
+++ src/client/routes/Dashboard/index.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+// import { useAppTranslation } from '@hooks';
+
+const DashboardView = styled.div`
+  display: flex;
+  flex: 1;
+`;
+
+export const Dashboard = () => {
+  // const { t } = useAppTranslation('common');
+
+  return <DashboardView>Dashboard</DashboardView>;
+};
--- src/client/routes/index.tsx
+++ src/client/routes/index.tsx
@@ -3,19 +3,22 @@ import { Layout } from '@containers';
 
 // import { Page404 } from './404';
 import { Save } from './Save';
+import { Invest } from './Invest';
 import { Landing } from './Landing';
+import { Dashboard } from './Dashboard';
+import { VaultDetail } from './VaultDetail';
 
 export const Routes = () => {
   return (
     <Router>
       <Layout>
         <Switch>
-          <Route exact path="/save">
-            <Save />
-          </Route>
-          <Route path="/">
-            <Landing />
-          </Route>
+          <Route exact path="/dashboard" component={Dashboard} />
+          <Route exact path="/invest" component={Invest} />
+          <Route exact path="/save" component={Save} />
+          <Route exact path="/vault/:vaultId" component={VaultDetail} />
+
+          <Route path="/" component={Landing} />
           <Route path="*">
             <Redirect to="/" />
           </Route>
