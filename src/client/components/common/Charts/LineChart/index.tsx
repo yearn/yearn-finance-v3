7c6371050a7f7806e23ed948c480cca0aaf66cc8
turtlemoji <turtlemoji@protonmail.com>
2021-04-28 16:21:19 -0300

    feat: add line-chart component

---
+++ src/client/components/common/Charts/LineChart/index.tsx
@@ -0,0 +1,74 @@
+import { FC } from 'react';
+import styled from 'styled-components';
+import { ResponsiveLine } from '@nivo/line';
+
+export interface LineChartProps {
+  className?: string;
+  data: any;
+}
+
+const StyledLineChart = styled.div``;
+
+export const LineChart: FC<LineChartProps> = ({ data, className, ...props }) => (
+  <StyledLineChart className={className} {...props}>
+    <ResponsiveLine
+      data={data}
+      margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
+      xScale={{ type: 'point' }}
+      yScale={{ type: 'linear', min: 'auto', max: 'auto', stacked: true, reverse: false }}
+      yFormat=" >-.2f"
+      axisTop={null}
+      axisRight={null}
+      axisBottom={{
+        orient: 'bottom',
+        tickSize: 5,
+        tickPadding: 5,
+        tickRotation: 0,
+        legend: 'transportation',
+        legendOffset: 36,
+        legendPosition: 'middle',
+      }}
+      axisLeft={{
+        orient: 'left',
+        tickSize: 5,
+        tickPadding: 5,
+        tickRotation: 0,
+        legend: 'count',
+        legendOffset: -40,
+        legendPosition: 'middle',
+      }}
+      pointSize={10}
+      pointColor={{ theme: 'background' }}
+      pointBorderWidth={2}
+      pointBorderColor={{ from: 'serieColor' }}
+      pointLabelYOffset={-12}
+      useMesh={true}
+      legends={[
+        {
+          anchor: 'bottom-right',
+          direction: 'column',
+          justify: false,
+          translateX: 100,
+          translateY: 0,
+          itemsSpacing: 0,
+          itemDirection: 'left-to-right',
+          itemWidth: 80,
+          itemHeight: 20,
+          itemOpacity: 0.75,
+          symbolSize: 12,
+          symbolShape: 'circle',
+          symbolBorderColor: 'rgba(0, 0, 0, .5)',
+          effects: [
+            {
+              on: 'hover',
+              style: {
+                itemBackground: 'rgba(0, 0, 0, .03)',
+                itemOpacity: 1,
+              },
+            },
+          ],
+        },
+      ]}
+    />
+  </StyledLineChart>
+);
