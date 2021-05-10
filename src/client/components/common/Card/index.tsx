6b791592c4ec7027b13bf13fe0c1198186debbba
xgambitox <xgmolinax@gmail.com>
2021-05-10 13:36:53 -0600

    feat: card components (#29)
    * feat: add common card components
    
    * feat: add summary card
    
    * feat: add detail card component
    
    * feat: card variant theme

--- src/client/components/common/Card/index.tsx
+++ src/client/components/common/Card/index.tsx
@@ -1,18 +1,45 @@
 import { FC } from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
+import { CardHeader } from './CardHeader';
+import { CardContent } from './CardContent';
+import { CardElement } from './CardElement';
 import { styledSystem, StyledSystemProps } from '../styledSystem';
+
+const defaultVariant = css`
+  background-color: ${({ theme }) => theme.colors.card};
+  color: ${({ theme }) => theme.contrasts.card};
+`;
+
+const primaryVariant = css`
+  background-color: ${({ theme }) => theme.colors.primary};
+  color: ${({ theme }) => theme.contrasts.primary};
+`;
+
+const variantStyle = ({ variant }: CardProps) => {
+  switch (variant) {
+    case 'primary':
+      return primaryVariant;
+    default:
+      return defaultVariant;
+  }
+};
+
+type CardVariant = 'primary';
 
 export interface CardProps extends StyledSystemProps {
   onClick?: () => void;
+  variant?: CardVariant;
 }
 
-const StyledDiv = styled.div<StyledSystemProps>`
-  background-color: ${({ theme }) => theme.colors.card};
-  color: ${({ theme }) => theme.contrasts.card};
+const StyledDiv = styled.div<CardProps>`
   border-radius: 1.5rem;
   padding: 1.7rem 1.1rem;
+  min-width: fit-content;
+  ${variantStyle};
   ${styledSystem};
 `;
 
 export const Card: FC<CardProps> = (props) => <StyledDiv {...props} />;
+
+export { CardHeader, CardContent, CardElement };
