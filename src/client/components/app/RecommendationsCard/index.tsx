2723d5b5b956a5fe873bb9308bcfd0f89591cbc6
xgambitox <xgmolinax@gmail.com>
2021-05-13 18:29:27 -0600

    feat: recommendations card (#36)

---
+++ src/client/components/app/RecommendationsCard/index.tsx
@@ -0,0 +1,84 @@
+import styled from 'styled-components';
+
+import { Card, CardHeader, CardContent, CardElement, Text, Button } from '@components/common';
+
+const ContainerCard = styled(Card)`
+  max-width: max-content;
+  padding: 1.2rem 1.2rem 0 0;
+  margin-bottom: 1.6rem;
+`;
+
+const ItemCard = styled(Card)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 25.6rem;
+  width: 18rem;
+  padding: 1.2rem 0;
+  margin-bottom: 1.6rem;
+`;
+
+const InnerBox = styled(Card)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 15.6rem;
+  width: 15.6rem;
+  padding: 1.2rem 0;
+  margin-bottom: 1.6rem;
+`;
+
+const CenterIcon = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 15.6rem;
+`;
+
+const Icon = styled.img`
+  width: 3.6rem;
+  height: 3.6rem;
+`;
+
+interface Item {
+  header: string;
+  icon: string;
+  name: string;
+  info: string;
+  infoDetail?: string;
+  action: string;
+  onAction: () => void;
+}
+
+interface RecommendationsProps {
+  header: string;
+  items: Item[];
+}
+
+export const RecomendationsCard = ({ header, items }: RecommendationsProps) => {
+  return (
+    <ContainerCard>
+      <CardHeader header={header} />
+      <CardContent wrap>
+        {items.map((item, i) => (
+          <CardElement key={`${i}-${item.name}`}>
+            <ItemCard variant="primary">
+              <InnerBox>
+                {item.header}
+                <CenterIcon>
+                  <Icon alt={item.name} src={item.icon} />
+                  {item.name}
+                </CenterIcon>
+              </InnerBox>
+              <Text fontSize="2.4rem" fontWeight="600">
+                {item.info}
+              </Text>
+              <Button onClick={item.onAction}>{item.action}</Button>
+            </ItemCard>
+          </CardElement>
+        ))}
+      </CardContent>
+    </ContainerCard>
+  );
+};
