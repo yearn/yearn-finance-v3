a0d2bdab0fe9fe5288bfe7f8f05f6dacf53996c2
turtlemoji <78362532+turtlemoji@users.noreply.github.com>
2021-06-03 21:50:30 -0300

    fix: default theme [WEB-291] (#65)
    * fix: default theme
    
    * refactor: default theme

--- src/core/store/modules/theme/theme.reducer.ts
+++ src/core/store/modules/theme/theme.reducer.ts
@@ -1,15 +1,18 @@
 import { createReducer } from '@reduxjs/toolkit';
 import { ThemeState } from '@types';
+import { getConfig } from '@config';
+
 import { ThemeActions } from './theme.actions';
 
 const initialState: ThemeState = {
-  current: 'default',
+  current: getConfig().DEFAULT_THEME,
 };
 
 const { changeTheme } = ThemeActions;
 
 const themeReducer = createReducer(initialState, (builder) => {
   builder.addCase(changeTheme, (state, { payload: { theme } }) => {
+    // TODO Check that the theme exists on AVAILABLE_THEMES
     state.current = theme;
   });
 });
