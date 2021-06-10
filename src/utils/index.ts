db3b6d026431f332c110fcb86e843b6866d18f1a
gotzenx <gotzenlenek@gmail.com>
2021-06-10 19:14:20 -0300

    chore: created new calculations util

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,3 +3,4 @@ export * from './format';
 export * from './misc';
 export * from './network';
 export * from './validations';
+export * from './calculations';
--- src/utils/validations.ts
+++ src/utils/validations.ts
@@ -121,17 +121,3 @@ export function validateVaultWithdrawAllowance(props: ValidateVaultWithdrawAllow
 
   return { approved: true };
 }
-
-interface CalculateSharesAmountProps {
-  decimals: string;
-  pricePerShare: string;
-  amount: BigNumber;
-}
-
-export function calculateSharesAmount(props: CalculateSharesAmountProps): Wei {
-  const { amount, decimals, pricePerShare } = props;
-  if (amount.isNaN()) return '0';
-  const sharePrice = formatUnits(pricePerShare, decimals);
-  const ONE_UNIT = new BigNumber(10).pow(decimals);
-  return amount.dividedBy(sharePrice).multipliedBy(ONE_UNIT).toFixed(0);
-}
