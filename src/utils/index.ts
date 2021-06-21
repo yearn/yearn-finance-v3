fedbf9f0b7b8f63221e3afdb93c754e505f6c68b
xgambitox <xgambitox@gmail.com>
2021-06-21 16:33:11 -0600

    feat: base backscratcher service [WEB-411] (#84)
    * feat: add http utils
    
    * feat: base backscratcher service
    
    * fix: cast strings
    
    * fix: add api validations
    
    * refactor: promise all optimizations
    
    * refactor: consistent token address

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,7 @@
 export * from './calculations';
 export * from './environment';
 export * from './format';
+export * from './http';
 export * from './misc';
 export * from './network';
 export * from './transaction';
