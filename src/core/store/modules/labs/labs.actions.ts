bfe92b13a29e2072de16df18e0cde8f0c41836cf
gotzenx <78360669+gotzenx@users.noreply.github.com>
2021-06-21 19:38:32 -0300

    Feat: fetch labs data + positions [WEB-425] (#86)
    * feat: get labs data
    
    * feat: get labs dynamic data
    
    * feat: get user labs positions

--- src/core/store/modules/labs/labs.actions.ts
+++ src/core/store/modules/labs/labs.actions.ts
@@ -1,11 +1,48 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { ThunkAPI } from '../../../frameworks/redux';
+import { Lab, LabDynamic, Position } from '@types';
 
 const initiateLabs = createAsyncThunk<void, string | undefined, ThunkAPI>(
   'labs/initiateLabs',
-  async (_arg, { dispatch }) => {}
+  async (_arg, { dispatch }) => {
+    await dispatch(getLabs());
+  }
 );
+
+const getLabs = createAsyncThunk<{ labsData: Lab[] }, void, ThunkAPI>('labs/getLabs', async (_arg, { extra }) => {
+  const { labService } = extra.services;
+  const labsData = await labService.getSupportedLabs();
+  return { labsData };
+});
+
+const getLabsDynamic = createAsyncThunk<{ labsDynamicData: LabDynamic[] }, { addresses: string[] }, ThunkAPI>(
+  'labs/getLabsDynamic',
+  async ({ addresses }, { extra }) => {
+    const { labService } = extra.services;
+    // const labsDynamicData = await labService.getLabsDynamicData(addresses); // TODO pass addresses. waitint to xgaminto to merge his stuff to avoid conficts y lab service
+    const labsDynamicData = await labService.getLabsDynamicData();
+    return { labsDynamicData };
+  }
+);
+
+const getUserLabsPositions = createAsyncThunk<
+  { userLabsPositions: Position[] },
+  { labsAddresses?: string[] },
+  ThunkAPI
+>('labs/getUserLabsPositions', async ({ labsAddresses }, { extra, getState }) => {
+  const { labService } = extra.services;
+  const userAddress = getState().wallet.selectedAddress;
+  if (!userAddress) {
+    throw new Error('WALLET NOT CONNECTED');
+  }
+  // const userLabsPositions = await labService.getUserLabsPositions({ userAddress, labsAddresses });
+  const userLabsPositions = await labService.getUserLabsPositions({ userAddress }); // TODO pass addresses. waitint to xgaminto to merge his stuff to avoid conficts y lab service
+  return { userLabsPositions };
+});
 
 export const LabsActions = {
   initiateLabs,
+  getLabs,
+  getLabsDynamic,
+  getUserLabsPositions,
 };
--- src/core/store/modules/labs/labs.reducer.ts
+++ src/core/store/modules/labs/labs.reducer.ts
@@ -1,5 +1,13 @@
 import { createReducer } from '@reduxjs/toolkit';
-import { initialStatus, LabsState, UserLabActionsStatusMap, LabActionsStatusMap } from '@types';
+import {
+  initialStatus,
+  LabsState,
+  UserLabActionsStatusMap,
+  LabActionsStatusMap,
+  Position,
+  LabsPositionsMap,
+} from '@types';
+import { groupBy, keyBy, union } from 'lodash';
 import { LabsActions } from './labs.actions';
 
 export const initialLabActionsStatusMap: LabActionsStatusMap = {
@@ -8,6 +16,7 @@ export const initialLabActionsStatusMap: LabActionsStatusMap = {
 
 export const initialUserLabsActionsStatusMap: UserLabActionsStatusMap = {
   get: initialStatus,
+  getPositions: initialStatus,
 };
 
 const initialState: LabsState = {
@@ -29,7 +38,7 @@ const initialState: LabsState = {
   },
 };
 
-const { initiateLabs } = LabsActions;
+const { initiateLabs, getLabs, getLabsDynamic, getUserLabsPositions } = LabsActions;
 
 const labsReducer = createReducer(initialState, (builder) => {
   builder
@@ -41,7 +50,96 @@ const labsReducer = createReducer(initialState, (builder) => {
     })
     .addCase(initiateLabs.rejected, (state, { error }) => {
       state.statusMap.initiateLabs = { error: error.message };
+    })
+    .addCase(getLabs.pending, (state) => {
+      state.statusMap.getLabs = { loading: true };
+    })
+    .addCase(getLabs.fulfilled, (state, { payload: { labsData } }) => {
+      const labsAddresses: string[] = [];
+      labsData.forEach((lab) => {
+        labsAddresses.push(lab.address);
+        state.labsMap[lab.address] = lab;
+        state.statusMap.labsActionsStatusMap[lab.address] = initialLabActionsStatusMap;
+        state.statusMap.user.userLabsActionsStatusMap[lab.address] = initialUserLabsActionsStatusMap;
+      });
+      state.labsAddresses = union(state.labsAddresses, labsAddresses);
+      state.statusMap.getLabs = {};
+    })
+    .addCase(getLabs.rejected, (state, { error }) => {
+      state.statusMap.getLabs = { error: error.message };
+    })
+    .addCase(getLabsDynamic.pending, (state, { meta }) => {
+      const labsAddresses = meta.arg.addresses;
+      labsAddresses.forEach((address) => {
+        state.statusMap.labsActionsStatusMap[address].get = { loading: true };
+      });
+    })
+    .addCase(getLabsDynamic.fulfilled, (state, { meta, payload: { labsDynamicData } }) => {
+      const labsAddresses = meta.arg.addresses;
+      labsAddresses.forEach((address) => (state.statusMap.labsActionsStatusMap[address].get = {}));
+
+      labsDynamicData.forEach((labDynamicData) => {
+        const labAddress = labDynamicData.address;
+        state.labsMap[labAddress] = {
+          ...state.labsMap[labAddress],
+          ...labDynamicData,
+        };
+      });
+    })
+    .addCase(getLabsDynamic.rejected, (state, { error, meta }) => {
+      const labsAddresses = meta.arg.addresses;
+      labsAddresses.forEach((address) => {
+        state.statusMap.labsActionsStatusMap[address].get = { error: error.message };
+      });
+    })
+    .addCase(getUserLabsPositions.pending, (state, { meta }) => {
+      const labsAddresses = meta.arg.labsAddresses || [];
+      labsAddresses.forEach((address) => {
+        checkAndInitUserLabStatus(state, address);
+        state.statusMap.user.userLabsActionsStatusMap[address].getPositions = { loading: true };
+      });
+      state.statusMap.user.getUserLabsPositions = { loading: true };
+    })
+    .addCase(getUserLabsPositions.fulfilled, (state, { meta, payload: { userLabsPositions } }) => {
+      const labsPositionsMap = parsePositionsIntoMap(userLabsPositions);
+      const labsAddresses = meta.arg.labsAddresses;
+      labsAddresses?.forEach((address) => {
+        state.statusMap.user.userLabsActionsStatusMap[address].getPositions = {};
+      });
+
+      userLabsPositions.forEach((position) => {
+        const address = position.assetAddress;
+        const allowancesMap: any = {};
+        position.assetAllowances.forEach((allowance) => (allowancesMap[allowance.spender] = allowance.amount));
+
+        state.user.labsAllowancesMap[address] = allowancesMap;
+      });
+
+      state.user.userLabsPositionsMap = { ...state.user.userLabsPositionsMap, ...labsPositionsMap };
+      state.statusMap.user.getUserLabsPositions = {};
+    })
+    .addCase(getUserLabsPositions.rejected, (state, { meta, error }) => {
+      const labsAddresses = meta.arg.labsAddresses || [];
+      labsAddresses.forEach((address) => {
+        state.statusMap.user.userLabsActionsStatusMap[address].getPositions = {};
+      });
+      state.statusMap.user.getUserLabsPositions = { error: error.message };
     });
 });
+
+function checkAndInitUserLabStatus(state: LabsState, labAddress: string) {
+  const actionsMap = state.statusMap.user.userLabsActionsStatusMap[labAddress];
+  if (actionsMap) return;
+  state.statusMap.user.userLabsActionsStatusMap[labAddress] = { ...initialUserLabsActionsStatusMap };
+}
+
+function parsePositionsIntoMap(positions: Position[]): { [labAddress: string]: LabsPositionsMap } {
+  const grouped = groupBy(positions, 'assetAddress');
+  const labsMap: { [labAddress: string]: any } = {};
+  Object.entries(grouped).forEach(([key, value]) => {
+    labsMap[key] = keyBy(value, 'typeId');
+  });
+  return labsMap;
+}
 
 export default labsReducer;
