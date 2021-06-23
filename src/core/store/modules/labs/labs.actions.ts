0c7bc6a06eb890c852697e204b713bb355ae9c7f
gotzenx <78360669+gotzenx@users.noreply.github.com>
2021-06-23 16:24:28 -0300

    Feat: get yve crv extra data [WEB-431] (#89)
    * feat: now getVaults actions can receive addresses[]
    
    * feat: get yveCrv extra needed data
    
    * chore: added await

--- src/core/store/modules/labs/labs.actions.ts
+++ src/core/store/modules/labs/labs.actions.ts
@@ -1,6 +1,8 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { ThunkAPI } from '../../../frameworks/redux';
 import { Lab, LabDynamic, Position } from '@types';
+import { VaultsActions } from '../vaults/vaults.actions';
+import { getConstants } from '../../../../config/constants';
 
 const initiateLabs = createAsyncThunk<void, string | undefined, ThunkAPI>(
   'labs/initiateLabs',
@@ -9,18 +11,24 @@ const initiateLabs = createAsyncThunk<void, string | undefined, ThunkAPI>(
   }
 );
 
-const getLabs = createAsyncThunk<{ labsData: Lab[] }, void, ThunkAPI>('labs/getLabs', async (_arg, { extra }) => {
-  const { labService } = extra.services;
-  const labsData = await labService.getSupportedLabs();
-  return { labsData };
-});
+const getLabs = createAsyncThunk<{ labsData: Lab[] }, void, ThunkAPI>(
+  'labs/getLabs',
+  async (_arg, { extra, dispatch }) => {
+    const { labService } = extra.services;
+    dispatch(getYveCrvExtraData({}));
+    const labsData = await labService.getSupportedLabs();
+    return { labsData };
+  }
+);
 
 const getLabsDynamic = createAsyncThunk<{ labsDynamicData: LabDynamic[] }, { addresses: string[] }, ThunkAPI>(
   'labs/getLabsDynamic',
-  async ({ addresses }, { extra }) => {
+  async ({ addresses }, { extra, dispatch }) => {
     const { labService } = extra.services;
-    // const labsDynamicData = await labService.getLabsDynamicData(addresses); // TODO pass addresses. waitint to xgaminto to merge his stuff to avoid conficts y lab service
-    const labsDynamicData = await labService.getLabsDynamicData();
+    const [labsDynamicData] = await Promise.all([
+      labService.getLabsDynamicData(), // TODO pass addresses. waitint to xgaminto to merge his stuff to avoid conficts y lab service
+      dispatch(getYveCrvExtraData({ fetchDynamicData: true })),
+    ]);
     return { labsDynamicData };
   }
 );
@@ -29,20 +37,44 @@ const getUserLabsPositions = createAsyncThunk<
   { userLabsPositions: Position[] },
   { labsAddresses?: string[] },
   ThunkAPI
->('labs/getUserLabsPositions', async ({ labsAddresses }, { extra, getState }) => {
+>('labs/getUserLabsPositions', async ({ labsAddresses }, { extra, getState, dispatch }) => {
   const { labService } = extra.services;
   const userAddress = getState().wallet.selectedAddress;
   if (!userAddress) {
     throw new Error('WALLET NOT CONNECTED');
   }
-  // const userLabsPositions = await labService.getUserLabsPositions({ userAddress, labsAddresses });
-  const userLabsPositions = await labService.getUserLabsPositions({ userAddress }); // TODO pass addresses. waitint to xgaminto to merge his stuff to avoid conficts y lab service
+  const [userLabsPositions] = await Promise.all([
+    labService.getUserLabsPositions({ userAddress }), // TODO pass addresses. waitint to xgaminto to merge his stuff to avoid conficts y lab service
+    dispatch(getUserYveCrvExtraData()),
+  ]);
   return { userLabsPositions };
 });
+
+const getYveCrvExtraData = createAsyncThunk<void, { fetchDynamicData?: boolean }, ThunkAPI>(
+  'labs/getYveCrvExtraData',
+  async ({ fetchDynamicData }, { dispatch }) => {
+    const YVTHREECRV = getConstants().CONTRACT_ADDRESSES.YVTHREECRV;
+    if (fetchDynamicData) {
+      await dispatch(VaultsActions.getVaultsDynamic({ addresses: [YVTHREECRV] }));
+      return;
+    }
+    await dispatch(VaultsActions.getVaults({ addresses: [YVTHREECRV] }));
+  }
+);
+
+const getUserYveCrvExtraData = createAsyncThunk<void, void, ThunkAPI>(
+  'labs/getUserYveCrvExtraData',
+  async (_args, { dispatch }) => {
+    const YVTHREECRV = getConstants().CONTRACT_ADDRESSES.YVTHREECRV;
+    await dispatch(VaultsActions.getUserVaultsPositions({ vaultAddresses: [YVTHREECRV] }));
+  }
+);
 
 export const LabsActions = {
   initiateLabs,
   getLabs,
   getLabsDynamic,
   getUserLabsPositions,
+  getYveCrvExtraData,
+  getUserYveCrvExtraData,
 };
