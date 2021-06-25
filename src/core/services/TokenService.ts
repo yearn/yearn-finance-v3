ca8db62cabb21c30b389db8fdd841defea381bf8
gotzenx <gotzenlenek@gmail.com>
2021-06-25 14:55:24 -0300

    feat: fetch yvboost token data

--- src/core/services/TokenService.ts
+++ src/core/services/TokenService.ts
@@ -3,6 +3,10 @@ import { TokenService, YearnSdk, TokenDynamicData, ApproveProps, Web3Provider, B
 import { getContract } from '@frameworks/ethers';
 import erc20Abi from './contracts/erc20.json';
 import { unionBy } from 'lodash';
+import { getConstants } from '../../config/constants';
+import yvBoostAbi from './contracts/yvBoost.json';
+import { JsonRpcProvider } from '@ethersproject/providers';
+import { get, toBN, USDC_DECIMALS } from '../../utils';
 
 export class TokenServiceImpl implements TokenService {
   private yearnSdk: YearnSdk;
@@ -15,12 +19,20 @@ export class TokenServiceImpl implements TokenService {
 
   public async getSupportedTokens(): Promise<Token[]> {
     const yearn = this.yearnSdk;
-    const [zapperTokens, vaultsTokens, ironBankTokens]: [Token[], Token[], Token[]] = await Promise.all([
+    const [zapperTokens, vaultsTokens, ironBankTokens, yvBoostToken]: [
+      Token[],
+      Token[],
+      Token[],
+      Token
+    ] = await Promise.all([
       yearn.tokens.supported(),
       yearn.vaults.tokens(),
       yearn.ironBank.tokens(),
+      getYvBoostToken(this.web3Provider.getInstanceOf('default')),
     ]);
+
     const tokens = unionBy(vaultsTokens, ironBankTokens, 'address');
+    tokens.push(yvBoostToken);
     return unionBy(zapperTokens, tokens, 'address');
   }
 
@@ -59,4 +71,31 @@ export class TokenServiceImpl implements TokenService {
     const receipt = await transaction.wait(1);
     console.log('Receipt: ', receipt);
   }
+}
+
+async function getYvBoostToken(provider: JsonRpcProvider): Promise<Token> {
+  // yvBoost
+  const YVBOOST = getConstants().CONTRACT_ADDRESSES.YVBOOST;
+  const yvBoostContract = getContract(YVBOOST, yvBoostAbi, provider);
+  const [decimals, name, symbol, pricesResponse] = await Promise.all([
+    yvBoostContract.decimals(),
+    yvBoostContract.name(),
+    yvBoostContract.symbol(),
+    get('https://api.coingecko.com/api/v3/simple/price?ids=yvboost&vs_currencies=usd'),
+  ]);
+  const yvBoostPrice = pricesResponse.data['yvboost']['usd'];
+  return {
+    address: yvBoostContract.address,
+    decimals: decimals.toString(),
+    name,
+    priceUsdc: toBN(yvBoostPrice)
+      .multipliedBy(10 ** USDC_DECIMALS)
+      .toString(),
+    supported: {
+      zapper: false,
+    },
+    symbol,
+    icon:
+      'https://raw.githubusercontent.com/yearn/yearn-assets/master/icons/tokens/0x9d409a0A012CFbA9B15F6D4B36Ac57A46966Ab9a/logo-128.png',
+  };
 }
