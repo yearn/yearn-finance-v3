8965b11da6408770cbbff31be1b4115165596ef8
xgambitox <xgambitox@gmail.com>
2021-07-07 14:42:52 -0600

    feat: add general transaction component

---
+++ src/client/components/app/Transactions/Transaction.tsx
@@ -0,0 +1,134 @@
+import { FC } from 'react';
+import styled from 'styled-components';
+
+import { TxActionButton, TxActions } from './components/TxActions';
+import { TxContainer } from './components/TxContainer';
+import { TxTokenInput } from './components/TxTokenInput';
+import { TxError } from './components/TxError';
+import { TxStatus } from './components/TxStatus';
+import { TxArrowStatus, TxArrowStatusTypes } from './components/TxArrowStatus';
+
+import { formatAmount } from '@src/utils';
+
+interface Status {
+  loading?: boolean;
+  error?: string;
+}
+
+interface Action {
+  label: string;
+  onAction: () => void;
+  status: Status;
+  disabled: boolean;
+}
+
+interface Asset {
+  address: string;
+  symbol: string;
+  icon: string;
+  balance: string;
+  decimals: number;
+  yield?: string;
+}
+
+interface TransactionProps {
+  transactionLabel: string;
+  transactionCompleted: boolean;
+  transactionCompletedLabel: string;
+  onTransactionCompletedDismissed: () => void;
+  sourceHeader: string;
+  sourceAssetOptions: Asset[];
+  selectedSourceAsset: Asset;
+  onSelectedSourceAssetChange?: (assetAddress: string) => void;
+  sourceAmount: string;
+  sourceAmountValue?: string;
+  onSourceAmountChange: (amount: string) => void;
+  targetHeader: string;
+  targetAssetOptions: Asset[];
+  selectedTargetAsset: Asset;
+  onSelectedTargetAssetChange?: (assetAddress: string) => void;
+  targetAmount: string;
+  targetAmountValue?: string;
+  targetAmountStatus: Status;
+  actions: Action[];
+  status: Status;
+  onClose?: () => void;
+}
+
+const StyledTransaction = styled(TxContainer)``;
+
+export const Transaction: FC<TransactionProps> = (props) => {
+  const {
+    transactionLabel,
+    transactionCompleted,
+    transactionCompletedLabel,
+    onTransactionCompletedDismissed,
+    sourceHeader,
+    sourceAssetOptions,
+    selectedSourceAsset,
+    onSelectedSourceAssetChange,
+    sourceAmount,
+    sourceAmountValue,
+    onSourceAmountChange,
+    targetHeader,
+    targetAssetOptions,
+    selectedTargetAsset,
+    onSelectedTargetAssetChange,
+    targetAmount,
+    targetAmountValue,
+    targetAmountStatus,
+    actions,
+    status,
+    onClose,
+  } = props;
+
+  const txStatus: TxArrowStatusTypes = 'preparing';
+
+  if (transactionCompleted) {
+    return (
+      <StyledTransaction onClose={onClose} header={transactionLabel} {...props}>
+        <TxStatus exit={onTransactionCompletedDismissed} />
+      </StyledTransaction>
+    );
+  }
+
+  return (
+    <StyledTransaction onClose={onClose} header={transactionLabel} {...props}>
+      <TxTokenInput
+        headerText={sourceHeader}
+        inputText={`Balance ${formatAmount(selectedSourceAsset.balance, 4)} ${selectedSourceAsset.symbol}`}
+        amount={sourceAmount}
+        onAmountChange={onSourceAmountChange}
+        amountValue={sourceAmountValue}
+        maxAmount={selectedSourceAsset.balance}
+        selectedToken={selectedSourceAsset}
+        tokenOptions={sourceAssetOptions}
+        onSelectedTokenChange={onSelectedSourceAssetChange}
+        inputError={!!status.error}
+      />
+
+      {!status.error && <TxArrowStatus status={txStatus} />}
+      {status.error && <TxError errorText={status.error} />}
+
+      <TxTokenInput
+        headerText={targetHeader}
+        inputText={`Balance ${formatAmount(selectedTargetAsset.balance, 4)} ${selectedTargetAsset.symbol}`}
+        amount={targetAmount}
+        amountValue={targetAmountValue}
+        selectedToken={selectedTargetAsset}
+        tokenOptions={targetAssetOptions}
+        onSelectedTokenChange={onSelectedTargetAssetChange}
+        yieldPercent={selectedTargetAsset.yield}
+        readOnly
+      />
+
+      <TxActions>
+        {actions.map(({ label, onAction, status, disabled }) => (
+          <TxActionButton onClick={onAction} disabled={disabled} pending={status.loading}>
+            {label}
+          </TxActionButton>
+        ))}
+      </TxActions>
+    </StyledTransaction>
+  );
+};
