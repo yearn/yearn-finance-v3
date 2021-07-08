f1b8437cf497ea74bc0f101d07146794d3dd8367
xgambitox <xgambitox@gmail.com>
2021-07-07 18:55:14 -0600

    refactor: transaction component (#126)
    * feat: add general transaction component
    
    * feat: add dispatch and unwrap hook
    
    * refactor: vault deposit modal
    
    * fix: deposit transaction complete
    
    * refactor: vault withdraw transaction modal
    
    * refactor: backscratcher lock modal

---
+++ src/client/components/app/Transactions/Transaction.tsx
@@ -0,0 +1,137 @@
+import { FC } from 'react';
+import styled from 'styled-components';
+
+import { TxActionButton, TxActions } from './components/TxActions';
+import { TxContainer } from './components/TxContainer';
+import { TxTokenInput } from './components/TxTokenInput';
+import { TxError } from './components/TxError';
+import { TxStatus } from './components/TxStatus';
+import { TxArrowStatus, TxArrowStatusTypes } from './components/TxArrowStatus';
+
+import { formatAmount, normalizeAmount } from '@src/utils';
+
+interface Status {
+  loading?: boolean;
+  error?: string | null;
+}
+
+interface Action {
+  label: string;
+  onAction: () => void;
+  status: Status;
+  disabled?: boolean;
+}
+
+interface Asset {
+  address: string;
+  symbol: string;
+  icon?: string;
+  balance: string;
+  decimals: number;
+  yield?: string;
+}
+
+interface TransactionProps {
+  transactionLabel: string;
+  transactionCompleted: boolean;
+  transactionCompletedLabel: string;
+  onTransactionCompletedDismissed: () => void;
+  sourceHeader: string;
+  sourceAssetOptions: Asset[];
+  selectedSourceAsset: Asset;
+  onSelectedSourceAssetChange?: (assetAddress: string) => void;
+  sourceAmount: string;
+  sourceAmountValue?: string;
+  onSourceAmountChange: (amount: string) => void;
+  targetHeader: string;
+  targetAssetOptions: Asset[];
+  selectedTargetAsset: Asset;
+  onSelectedTargetAssetChange?: (assetAddress: string) => void;
+  targetAmount: string;
+  targetAmountValue?: string;
+  targetAmountStatus: Status;
+  actions: Action[];
+  status: Status;
+  onClose?: () => void;
+}
+
+const StyledTransaction = styled(TxContainer)``;
+
+export const Transaction: FC<TransactionProps> = (props) => {
+  const {
+    transactionLabel,
+    transactionCompleted,
+    transactionCompletedLabel,
+    onTransactionCompletedDismissed,
+    sourceHeader,
+    sourceAssetOptions,
+    selectedSourceAsset,
+    onSelectedSourceAssetChange,
+    sourceAmount,
+    sourceAmountValue,
+    onSourceAmountChange,
+    targetHeader,
+    targetAssetOptions,
+    selectedTargetAsset,
+    onSelectedTargetAssetChange,
+    targetAmount,
+    targetAmountValue,
+    targetAmountStatus,
+    actions,
+    status,
+    onClose,
+  } = props;
+
+  const sourceBalance = normalizeAmount(selectedSourceAsset.balance, selectedSourceAsset.decimals);
+  const targetBalance = normalizeAmount(selectedTargetAsset.balance, selectedTargetAsset.decimals);
+
+  const txStatus: TxArrowStatusTypes = 'preparing';
+
+  if (transactionCompleted) {
+    return (
+      <StyledTransaction onClose={onClose} header={transactionLabel} {...props}>
+        <TxStatus exit={onTransactionCompletedDismissed} />
+      </StyledTransaction>
+    );
+  }
+
+  return (
+    <StyledTransaction onClose={onClose} header={transactionLabel} {...props}>
+      <TxTokenInput
+        headerText={sourceHeader}
+        inputText={`Balance ${formatAmount(sourceBalance, 4)} ${selectedSourceAsset.symbol}`}
+        amount={sourceAmount}
+        onAmountChange={onSourceAmountChange}
+        amountValue={sourceAmountValue}
+        maxAmount={sourceBalance}
+        selectedToken={selectedSourceAsset}
+        tokenOptions={sourceAssetOptions}
+        onSelectedTokenChange={onSelectedSourceAssetChange}
+        inputError={!!status.error}
+      />
+
+      {!status.error && <TxArrowStatus status={txStatus} />}
+      {status.error && <TxError errorText={status.error} />}
+
+      <TxTokenInput
+        headerText={targetHeader}
+        inputText={`Balance ${formatAmount(targetBalance, 4)} ${selectedTargetAsset.symbol}`}
+        amount={targetAmount}
+        amountValue={targetAmountValue}
+        selectedToken={selectedTargetAsset}
+        tokenOptions={targetAssetOptions}
+        onSelectedTokenChange={onSelectedTargetAssetChange}
+        yieldPercent={selectedTargetAsset.yield}
+        readOnly
+      />
+
+      <TxActions>
+        {actions.map(({ label, onAction, status, disabled }) => (
+          <TxActionButton onClick={onAction} disabled={disabled} pending={status.loading}>
+            {label}
+          </TxActionButton>
+        ))}
+      </TxActions>
+    </StyledTransaction>
+  );
+};
--- src/client/components/app/Transactions/WithdrawTx.tsx
+++ src/client/components/app/Transactions/WithdrawTx.tsx
@@ -1,21 +1,11 @@
 import { FC, useState, useEffect } from 'react';
-import styled from 'styled-components';
 import { keyBy } from 'lodash';
 
-import { useAppSelector, useAppDispatch } from '@hooks';
+import { useAppSelector, useAppDispatch, useAppDispatchAndUnwrap } from '@hooks';
 import { TokensSelectors, VaultsSelectors, VaultsActions, TokensActions } from '@store';
-
-import { TxActionButton, TxActions } from './components/TxActions';
-import { TxContainer } from './components/TxContainer';
-import { TxTokenInput } from './components/TxTokenInput';
-import { TxError } from './components/TxError';
-import { TxStatus } from './components/TxStatus';
-import { TxArrowStatus, TxArrowStatusTypes } from './components/TxArrowStatus';
-
 import {
   toBN,
   formatPercent,
-  formatAmount,
   normalizeAmount,
   USDC_DECIMALS,
   validateVaultWithdraw,
@@ -24,11 +14,10 @@ import {
 } from '@src/utils';
 import { getConfig } from '@config';
 
+import { Transaction } from './Transaction';
 export interface WithdrawTxProps {
   onClose?: () => void;
 }
-
-const StyledWithdrawTx = styled(TxContainer)``;
 
 export const WithdrawTx: FC<WithdrawTxProps> = ({ onClose, children, ...props }) => {
   const { SLIPPAGE_OPTIONS, CONTRACT_ADDRESSES } = getConfig();
@@ -37,6 +26,7 @@ export const WithdrawTx: FC<WithdrawTxProps> = ({ onClose, children, ...props })
     label: formatPercent(value.toString(), 0),
   }));
   const dispatch = useAppDispatch();
+  const dispatchAndUnwrap = useAppDispatchAndUnwrap();
   const [amount, setAmount] = useState('');
   const [txCompleted, setTxCompleted] = useState(false);
   const selectedVault = useAppSelector(VaultsSelectors.selectSelectedVault);
@@ -115,8 +105,13 @@ export const WithdrawTx: FC<WithdrawTxProps> = ({ onClose, children, ...props })
   // TODO: NEED A CLEAR ERROR ACTION ON MODAL UNMOUNT
   const error = allowanceError || inputError || actionsStatus.approveZapOut.error || actionsStatus.withdraw.error;
 
-  const balance = normalizeAmount(selectedTargetToken.balance, selectedVault.token.decimals);
-  const vaultBalance = normalizeAmount(selectedVault.DEPOSIT.userDeposited, selectedVault.token.decimals);
+  const selectedVaultOption = {
+    address: selectedVault.address,
+    symbol: selectedVault.token.name,
+    icon: selectedVault.token.icon,
+    balance: selectedVault.DEPOSIT.userDeposited,
+    decimals: selectedVault.token.decimals,
+  };
   const amountValue = toBN(amount).times(normalizeAmount(selectedVault.token.priceUsdc, USDC_DECIMALS)).toString();
   const expectedAmount = toBN(amount).gt(0)
     ? normalizeAmount(expectedTxOutcome?.targetUnderlyingTokenAmount, selectedVault?.token.decimals)
@@ -125,11 +120,22 @@ export const WithdrawTx: FC<WithdrawTxProps> = ({ onClose, children, ...props })
     .times(normalizeAmount(selectedVault.token.priceUsdc, USDC_DECIMALS))
     .toString();
 
-  const approve = () => dispatch(VaultsActions.approveZapOut({ vaultAddress: selectedVault.address }));
+  const onSelectedTargetTokenChange = (tokenAddress: string) => {
+    setAmount('');
+    setSelectedTargetTokenAddress(tokenAddress);
+  };
 
-  const withdraw = () => {
+  const onTransactionCompletedDismissed = () => {
+    if (onClose) onClose();
+  };
+
+  const approve = async () => {
+    await dispatch(VaultsActions.approveZapOut({ vaultAddress: selectedVault.address }));
+  };
+
+  const withdraw = async () => {
     try {
-      dispatch(
+      await dispatchAndUnwrap(
         VaultsActions.withdrawVault({
           vaultAddress: selectedVault.address,
           amount: toBN(amount),
@@ -138,75 +144,46 @@ export const WithdrawTx: FC<WithdrawTxProps> = ({ onClose, children, ...props })
         })
       );
       setTxCompleted(true);
-    } catch (error) {
-      console.log(error);
-    }
+    } catch (error) {}
   };
 
-  const onSelectedTargetTokenChange = (tokenAddress: string) => {
-    setAmount('');
-    setSelectedTargetTokenAddress(tokenAddress);
-  };
-
-  const txStatus: TxArrowStatusTypes = 'preparing';
-
-  if (txCompleted) {
-    return (
-      <StyledWithdrawTx onClose={onClose} header="Withdraw" {...props}>
-        <TxStatus exit={() => setTxCompleted(false)} />
-      </StyledWithdrawTx>
-    );
-  }
+  const txActions = [
+    {
+      label: 'Approve',
+      onAction: approve,
+      status: actionsStatus.approveZapOut,
+      disabled: isApproved,
+    },
+    {
+      label: 'Withdraw',
+      onAction: withdraw,
+      status: actionsStatus.withdraw,
+      disabled: !isApproved || !isValidAmount,
+    },
+  ];
 
   return (
-    <StyledWithdrawTx onClose={onClose} header="Withdraw" {...props}>
-      <TxTokenInput
-        headerText="From vault"
-        inputText={`Balance ${formatAmount(vaultBalance, 4)} ${selectedVault.token.symbol}`}
-        amount={amount}
-        onAmountChange={setAmount}
-        amountValue={amountValue}
-        maxAmount={vaultBalance}
-        selectedToken={selectedVault.token}
-        tokenOptions={[
-          {
-            address: selectedVault.address,
-            symbol: selectedVault.name,
-            icon: selectedVault.token.icon,
-            balance: selectedVault.DEPOSIT.userDeposited,
-            decimals: selectedVault.token.decimals,
-          },
-        ]}
-        inputError={!!error?.length}
-      />
-
-      {!error && <TxArrowStatus status={txStatus} />}
-      {error && <TxError errorText={error} />}
-
-      <TxTokenInput
-        headerText="To wallet"
-        inputText={`Balance ${formatAmount(balance, 4)} ${selectedTargetToken.symbol}`}
-        amount={expectedAmount}
-        amountValue={expectedAmountValue}
-        selectedToken={selectedTargetToken}
-        onSelectedTokenChange={onSelectedTargetTokenChange}
-        tokenOptions={targetTokensOptions}
-        readOnly
-      />
-
-      <TxActions>
-        <TxActionButton onClick={() => approve()} disabled={isApproved} pending={actionsStatus.approveZapOut.loading}>
-          Approve
-        </TxActionButton>
-
-        <TxActionButton
-          onClick={() => withdraw()}
-          disabled={!isApproved || !isValidAmount}
-          pending={actionsStatus.withdraw.loading}
-        >
-          Withdraw
-        </TxActionButton>
-      </TxActions>
-    </StyledWithdrawTx>
+    <Transaction
+      transactionLabel="Withdraw"
+      transactionCompleted={txCompleted}
+      transactionCompletedLabel="Exit"
+      onTransactionCompletedDismissed={onTransactionCompletedDismissed}
+      sourceHeader="From vault"
+      sourceAssetOptions={[selectedVaultOption]}
+      selectedSourceAsset={selectedVaultOption}
+      sourceAmount={amount}
+      sourceAmountValue={amountValue}
+      onSourceAmountChange={setAmount}
+      targetHeader="To wallet"
+      targetAssetOptions={targetTokensOptions}
+      selectedTargetAsset={selectedTargetToken}
+      onSelectedTargetAssetChange={onSelectedTargetTokenChange}
+      targetAmount={expectedAmount}
+      targetAmountValue={expectedAmountValue}
+      targetAmountStatus={expectedTxOutcomeStatus}
+      actions={txActions}
+      status={{ error }}
+      onClose={onClose}
+    />
   );
 };
