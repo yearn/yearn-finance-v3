04eb919f942b41c742db46705e67b3df27e05de7
turtlemoji <78362532+turtlemoji@users.noreply.github.com>
2021-07-08 21:42:07 -0300

    feat: info cards (#127)

--- src/client/components/app/InfoCard/index.tsx
+++ src/client/components/app/InfoCard/index.tsx
@@ -1,11 +1,18 @@
 import { ReactNode } from 'react';
 import styled from 'styled-components';
 
-import { Card, CardHeader, CardContent, CardElement } from '@components/common';
+import { Card, CardHeader, CardContent, CardSizeType } from '@components/common';
+
+const StyledCardContent = styled(CardContent)`
+  margin: ${({ theme }) => theme.cardPadding};
+  font-size: 1.4rem;
+  color: inherit;
+`;
 
 const StyledCard = styled(Card)`
   max-width: max-content;
   padding: ${({ theme }) => theme.cardPadding} 0;
+  min-width: 36.5rem;
 `;
 
 interface InfoCardProps {
@@ -13,17 +20,17 @@ interface InfoCardProps {
   content?: string;
   Component?: ReactNode;
   variant?: 'primary' | 'secondary';
+  cardSize?: CardSizeType;
 }
 
-export const InfoCard = ({ header, content, Component, variant, ...props }: InfoCardProps) => {
+export const InfoCard = ({ header, content, Component, variant, cardSize, ...props }: InfoCardProps) => {
   return (
-    <StyledCard variant={variant} {...props}>
+    <StyledCard variant={variant} cardSize={cardSize} {...props}>
       <CardHeader header={header} />
-      <CardContent>
-        <CardElement content={content} width="36.5rem">
-          {Component}
-        </CardElement>
-      </CardContent>
+      <StyledCardContent>
+        {content}
+        {Component}
+      </StyledCardContent>
     </StyledCard>
   );
 };
