67726639991a8cbfff87da18d20d68b36d6a656e
gotzenx <gotzenlenek@gmail.com>
2021-07-15 15:40:09 -0300

    chore: console log env zapper key for testing

--- src/utils/environment.ts
+++ src/utils/environment.ts
@@ -4,4 +4,6 @@ export const isProd = () => getConfig().ENV === 'production';
 
 export const isDev = () => getConfig().ENV === 'development';
 
+console.log({ ZAPPER_API: getConfig().ZAPPER_API_KEY });
+
 export const enableDevTools = () => isDev() || getConfig().ALLOW_DEV_MODE;
