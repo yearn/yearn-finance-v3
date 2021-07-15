10f6b2a46e2746a9051a1b37bc9feb0c4caebead
turtlemoji <78362532+turtlemoji@users.noreply.github.com>
2021-07-15 11:10:06 -0300

    feat: comming soon modal (#151)

---
+++ src/client/containers/Modals/ComingSoonModal/index.tsx
@@ -0,0 +1,62 @@
+import { FC } from 'react';
+import styled from 'styled-components';
+
+import { Text, ConstructionIcon, Icon, Modal, Button } from '@components/common';
+
+const StyledButton = styled(Button)`
+  background-color: ${({ theme }) => theme.colors.txModalColors.loading};
+  text-transform: uppercase;
+`;
+
+const StyledIcon = styled(Icon)`
+  fill: currentColor;
+  width: 9.4rem;
+`;
+
+const StyledText = styled(Text)`
+  font-size: 2.4rem;
+  font-weight: 600;
+`;
+
+const TextContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 7rem;
+  padding: 9rem 5rem;
+  background-color: ${({ theme }) => theme.colors.txModalColors.backgroundVariant};
+  border-radius: ${({ theme }) => theme.globalRadius};
+`;
+
+const StyledComingSoonModal = styled(Modal)`
+  display: flex;
+  flex-direction: column;
+  gap: ${({ theme }) => theme.txModal.gap};
+
+  width: ${({ theme }) => theme.txModal.width};
+  height: auto;
+  color: ${({ theme }) => theme.colors.txModalColors.loading};
+  padding: ${({ theme }) => theme.txModal.gap};
+  border-radius: ${({ theme }) => theme.globalRadius};
+`;
+
+export interface ComingSoonModalProps {
+  onClose: () => void;
+  modalProps: {
+    testVar: string;
+  };
+}
+
+export const ComingSoonModal: FC<ComingSoonModalProps> = ({ onClose, modalProps, ...props }) => {
+  return (
+    <StyledComingSoonModal {...props}>
+      <TextContainer>
+        <StyledText>Coming Soon</StyledText>
+        <StyledIcon Component={ConstructionIcon} />
+      </TextContainer>
+
+      <StyledButton onClick={onClose}>Exit</StyledButton>
+    </StyledComingSoonModal>
+  );
+};
