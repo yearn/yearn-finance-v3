83a495d98d3eb49bc3b8990b035cafdb2279a920
gotzenx <gotzenlenek@gmail.com>
2021-07-16 12:45:52 -0300

    chore: remove zapper key log

--- src/utils/environment.ts
+++ src/utils/environment.ts
@@ -4,6 +4,4 @@ export const isProd = () => getConfig().ENV === 'production';
 
 export const isDev = () => getConfig().ENV === 'development';
 
-console.log({ ZAPPER_API: getConfig().ZAPPER_API_KEY });
-
 export const enableDevTools = () => isDev() || getConfig().ALLOW_DEV_MODE;
