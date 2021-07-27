1ce1257dc51c00d2d5f7e9a82844666634f8941a
gotzenx <gotzenlenek@gmail.com>
2021-07-27 16:46:54 -0300

    feat: repay modal validations

--- src/client/components/app/Transactions/IronBank/RepayTx.tsx
+++ src/client/components/app/Transactions/IronBank/RepayTx.tsx
@@ -2,7 +2,7 @@ import { FC, useState, useEffect } from 'react';
 
 import { useAppSelector, useAppDispatch, useAppDispatchAndUnwrap } from '@hooks';
 import { IronBankSelectors, IronBankActions } from '@store';
-import { toBN, normalizeAmount, normalizePercent, USDC_DECIMALS } from '@src/utils';
+import { toBN, normalizeAmount, normalizePercent, USDC_DECIMALS, basicValidateAmount, toWei } from '@src/utils';
 
 import { IronBankTransaction } from '../IronBankTransaction';
 
@@ -16,6 +16,7 @@ export const IronBankRepayTx: FC<IronBankRepayTxProps> = ({ onClose }) => {
   const [amount, setAmount] = useState('');
   const [txCompleted, setTxCompleted] = useState(false);
   const selectedMarket = useAppSelector(IronBankSelectors.selectSelectedMarket);
+  const selectedToken = selectedMarket?.token;
   const userIronBankSummary = useAppSelector(IronBankSelectors.selectUserIronBankSummary);
   const actionsStatus = useAppSelector(IronBankSelectors.selectSelectedMarketActionsStatusMap);
 
@@ -29,30 +30,33 @@ export const IronBankRepayTx: FC<IronBankRepayTxProps> = ({ onClose }) => {
     };
   }, []);
 
-  if (!selectedMarket || !userIronBankSummary) {
+  if (!selectedMarket || !userIronBankSummary || !selectedToken) {
     return null;
   }
 
-  // TODO: validations
-  const { approved: isValidAmount, error: inputError } = { approved: true, error: undefined };
-
-  const error = inputError || actionsStatus.repay.error;
-
   const borrowBalance = normalizeAmount(userIronBankSummary.borrowBalanceUsdc, USDC_DECIMALS);
-  const underlyingTokenPrice = normalizeAmount(selectedMarket.token.priceUsdc, USDC_DECIMALS);
+  const underlyingTokenPrice = normalizeAmount(selectedToken.priceUsdc, USDC_DECIMALS);
   const amountValue = toBN(amount).times(underlyingTokenPrice).toString();
   const borrowLimit = normalizeAmount(userIronBankSummary.borrowLimitUsdc, USDC_DECIMALS);
 
   const proyectedBorrowBalance = toBN(borrowBalance).minus(amountValue).toString();
-  const borrowedTokens = normalizeAmount(selectedMarket.BORROW.userDeposited, selectedMarket.token.decimals);
-  const tokenBalance = normalizeAmount(selectedMarket.token.balance, selectedMarket.token.decimals);
+  const borrowedTokens = normalizeAmount(selectedMarket.BORROW.userDeposited, selectedToken.decimals);
+  const tokenBalance = normalizeAmount(selectedToken.balance, selectedToken.decimals);
   const repayableTokens = toBN(tokenBalance).gt(borrowedTokens) ? borrowedTokens : tokenBalance;
 
   const asset = {
-    ...selectedMarket.token,
+    ...selectedToken,
     balance: selectedMarket.BORROW.userDeposited,
     yield: normalizePercent(selectedMarket.borrowApy, 2),
   };
+
+  const { approved: isValidAmount, error: inputError } = basicValidateAmount({
+    sellTokenAmount: toBN(amount),
+    sellTokenDecimals: selectedToken.decimals.toString(),
+    maxAmount: toWei(repayableTokens, selectedToken.decimals),
+  });
+
+  const error = inputError || actionsStatus.repay.error;
 
   const onTransactionCompletedDismissed = () => {
     if (onClose) onClose();
