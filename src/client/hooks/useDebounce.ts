afd0113ad582e96731fea3c5b09fa0e019a55cc5
gotzenx <gotzenlenek@gmail.com>
2021-07-30 15:52:40 -0300

    chore: removed log

--- src/client/hooks/useDebounce.ts
+++ src/client/hooks/useDebounce.ts
@@ -3,8 +3,6 @@ import { useEffect, useState } from 'react';
 export function useDebounce(value: string, delay: number) {
   const [debouncedValue, setDebouncedValue] = useState(value);
   useEffect(() => {
-    console.log('EFFECT');
-
     const handler = setTimeout(() => {
       setDebouncedValue(value);
     }, delay);
