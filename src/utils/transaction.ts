130d76c1d1c89302bf0c33bc68ce1ac5c2528e3d
xgambitox <xgambitox@gmail.com>
2021-08-05 15:30:33 -0600

    feat: notification handler

--- src/utils/transaction.ts
+++ src/utils/transaction.ts
@@ -1,9 +1,22 @@
 import { notify } from '@frameworks/blocknative';
 
-import { TransactionResponse } from '@types';
+import { TransactionResponse, TransactionReceipt } from '@types';
 
-export const handleTransaction = async (tx: TransactionResponse) => {
-  notify.hash(tx.hash);
-  const receipt = await tx.wait(1);
-  return receipt;
+export const handleTransaction = async (tx: TransactionResponse): Promise<TransactionReceipt> => {
+  try {
+    notify.hash(tx.hash);
+    const receipt = await tx.wait();
+    return receipt;
+  } catch (error) {
+    if (error.code === 'TRANSACTION_REPLACED') {
+      if (error.cancelled) {
+        throw new Error('Transaction Cancelled');
+      } else {
+        // notify.unsubscribe(tx.hash); TODO: VERIFY IF NOTIFICATION CHANGES AUTOMATICALLY
+        return await handleTransaction(error.replacement);
+      }
+    }
+
+    throw error;
+  }
 };
