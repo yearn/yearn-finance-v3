3bbe6d4581b9b3463ced882b070a84602ec9e971
xgambitox <xgambitox@gmail.com>
2021-09-07 17:44:09 -0600

    chore: remove console warning

--- src/core/frameworks/ethers/Web3Provider.ts
+++ src/core/frameworks/ethers/Web3Provider.ts
@@ -9,11 +9,11 @@ export class EthersWeb3ProviderImpl implements AppWeb3Provider {
   private instances: Map<ProviderType, JsonRpcProvider> = new Map<ProviderType, JsonRpcProvider>();
 
   constructor({ providerType }: { providerType: ProviderType }) {
-    const { FANTOM_PROVIDER_HTTPS, WEB3_PROVIDER_HTTPS, CUSTOM_PROVIDER_HTTPS } = getConfig();
+    const { FANTOM_PROVIDER_HTTPS, WEB3_PROVIDER_HTTPS, CUSTOM_PROVIDER_HTTPS, USE_MAINNET_FORK } = getConfig();
     this.providerType = providerType;
     this.register('default', getJsonRpcProvider(WEB3_PROVIDER_HTTPS));
     this.register('fantom', getJsonRpcProvider(FANTOM_PROVIDER_HTTPS));
-    this.register('custom', getJsonRpcProvider(CUSTOM_PROVIDER_HTTPS));
+    if (USE_MAINNET_FORK) this.register('custom', getJsonRpcProvider(CUSTOM_PROVIDER_HTTPS));
   }
 
   public getInstanceOf(type: ProviderType): JsonRpcProvider {
