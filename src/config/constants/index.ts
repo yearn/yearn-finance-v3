087b53a53397d332ae8b5808af11822dba1eed1a
xgambitox <18649057+xgambitox@users.noreply.github.com>
2021-10-04 15:27:30 -0600

    chore: disable multichain support

--- src/config/constants/index.ts
+++ src/config/constants/index.ts
@@ -51,7 +51,7 @@ export const getConstants = memoize((): Constants => {
     ETHEREUM_ADDRESS: ADDRESSES.ETH,
     MAX_UINT256: '115792089237316195423570985008687907853269984665640564039457584007913129639935',
     YEARN_API: 'https://api.yearn.finance/v1/chains/1/vaults/all',
-    SUPPORTED_NETWORKS: ['mainnet', 'fantom'],
+    SUPPORTED_NETWORKS: ['mainnet'],
     NETWORK_SETTINGS,
     WEB3_PROVIDER_HTTPS: `https://eth-mainnet.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
     WEB3_PROVIDER_WSS: `wss://eth-mainnet.ws.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
