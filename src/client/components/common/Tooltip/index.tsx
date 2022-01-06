98d90ab2173a0ca0d0db1629f50c97316d4f6b15
FiboApe <fiboape@gmail.com>
2022-01-06 11:10:19 -0300

    feat(common/components): add Tooltip component

---
+++ src/client/components/common/Tooltip/index.tsx
@@ -0,0 +1,138 @@
+import React, { FC, useRef, useState } from 'react';
+import { usePopper } from 'react-popper';
+import preventOverflow from '@popperjs/core/lib/modifiers/preventOverflow.js';
+import flip from '@popperjs/core/lib/modifiers/flip.js';
+import styled from 'styled-components';
+
+const StyledTooltipArrow = styled.div`
+  position: absolute;
+  width: 14px;
+  height: 14px;
+  background: inherit;
+  visibility: hidden;
+  &:before {
+    position: absolute;
+    width: 14px;
+    height: 14px;
+    background: inherit;
+    visibility: visible;
+    content: '';
+    transform: rotate(45deg);
+  }
+`;
+
+const StyledTooltip = styled.div`
+  --dropdown-background: ${({ theme }) => theme.colors.background};
+  --dropdown-color: ${({ theme }) => theme.colors.onSurfaceH2};
+  --dropdown-hover-color: ${({ theme }) => theme.colors.secondary};
+  --dropdown-selected-color: ${({ theme }) => theme.colors.surface};
+  --dropdown-selected-background: ${({ theme }) => theme.colors.onSurfaceH2};
+
+  background: var(--dropdown-background);
+  color: var(--dropdown-color);
+  fill: currentColor;
+  stroke: currentColor;
+  user-select: none;
+  border-radius: ${({ theme }) => theme.globalRadius};
+  position: relative;
+  font-size: 1.6rem;
+  padding: 0.8rem;
+  width: max-content;
+  min-width: 11rem;
+
+  &[data-popper-placement^='top'] > ${StyledTooltipArrow} {
+    bottom: -7px;
+  }
+
+  &[data-popper-placement^='bottom'] > ${StyledTooltipArrow} {
+    top: -7px;
+  }
+
+  &[data-popper-placement^='left'] > ${StyledTooltipArrow} {
+    right: -7px;
+  }
+
+  &[data-popper-placement^='right'] > ${StyledTooltipArrow} {
+    left: -7px;
+  }
+`;
+
+function composeEventHandler(handler: (event: string) => void, eventHandler: (event: string) => void) {
+  return (event: string) => {
+    if (eventHandler) {
+      eventHandler(event);
+    }
+    handler(event);
+  };
+}
+
+const Tooltip: FC<{
+  children: React.ReactElement;
+  tooltipComponent: React.ReactNode;
+}> = ({ children, tooltipComponent }) => {
+  const referenceElement = useRef<any>(null);
+  const popperElement = useRef<any>(null);
+  const arrowElement = useRef<any>(null);
+  const [open, setOpen] = useState(false);
+  const { styles, attributes, update } = usePopper(referenceElement.current, popperElement.current, {
+    placement: 'bottom',
+    modifiers: [
+      preventOverflow,
+      flip,
+      {
+        name: 'offset',
+        options: {
+          offset: [0, 14],
+        },
+      },
+      {
+        name: 'arrow',
+        options: {
+          element: arrowElement.current,
+        },
+      },
+    ],
+  });
+  const childrenProps = {
+    ref: referenceElement,
+    ...(children ? children.props && children.props : {}),
+  };
+
+  const handleOpen = () => {
+    setOpen(true);
+    update && update();
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+    update && update();
+  };
+
+  console.log(children, referenceElement, popperElement);
+
+  childrenProps.onMouseOver = composeEventHandler(handleOpen, childrenProps.onMouseOver);
+  childrenProps.onMouseLeave = composeEventHandler(handleClose, childrenProps.onMouseLeave);
+  childrenProps.onFocus = composeEventHandler(handleOpen, childrenProps.onFocus);
+  childrenProps.onBlur = composeEventHandler(handleClose, childrenProps.onBlur);
+  childrenProps.onTouchStart = handleOpen;
+  childrenProps.onTouchEnd = handleClose;
+
+  return (
+    <>
+      {React.cloneElement(children, childrenProps)}
+      <StyledTooltip
+        ref={popperElement}
+        style={{
+          ...styles.popper,
+          display: !open ? 'none' : undefined,
+        }}
+        {...attributes.popper}
+      >
+        {tooltipComponent}
+        <StyledTooltipArrow ref={arrowElement} style={styles.arrow} data-popper-arrow />
+      </StyledTooltip>
+    </>
+  );
+};
+
+export default Tooltip;
