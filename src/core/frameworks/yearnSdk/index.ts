ac8e089c4e54782bd4356e76a5d77ccbfa8cfcd7
FiboApe <fiboape@gmail.com>
2022-01-21 11:56:00 -0300

    refactor(yearnsdk): use only one instance at a time of the yearn sdk

--- src/core/frameworks/yearnSdk/index.ts
+++ src/core/frameworks/yearnSdk/index.ts
@@ -4,30 +4,40 @@ import { getNetworkId, getProviderType } from '@utils';
 import { YearnSdk, SdkNetwork, Web3Provider, Network, Config } from '@types';
 
 export class YearnSdkImpl implements YearnSdk {
-  private instances: Map<Network, Yearn<SdkNetwork>> = new Map<Network, Yearn<SdkNetwork>>();
+  private instance: Yearn<SdkNetwork> | null;
+  private SUPPORTED_NETWORKS: Network[];
+  private web3Provider: Web3Provider;
+  private currentNetwork: Network | null;
 
   constructor({ web3Provider, config }: { web3Provider: Web3Provider; config: Config }) {
     const { SUPPORTED_NETWORKS } = config;
 
-    SUPPORTED_NETWORKS.forEach((network) => {
-      const providerType = getProviderType(network);
-      const provider = web3Provider.getInstanceOf(providerType);
-      const networkId = getNetworkId(network) as SdkNetwork;
-      const sdkInstance = new Yearn(networkId, {
-        provider,
-      });
-      this.register(network, sdkInstance);
-    });
+    this.web3Provider = web3Provider;
+    this.SUPPORTED_NETWORKS = SUPPORTED_NETWORKS;
+    this.currentNetwork = null;
+    this.instance = null;
   }
 
-  public hasInstanceOf(network: Network): boolean {
-    return this.instances.has(network);
+  public hasInstanceOf(network: Network) {
+    return network === this.currentNetwork || !this.instance;
   }
 
   public getInstanceOf(network: Network): Yearn<SdkNetwork> {
-    const instance = this.instances.get(network);
+    const instance = this.instance;
 
-    if (!instance) {
+    if (!instance || network !== this.currentNetwork) {
+      if (this.SUPPORTED_NETWORKS.includes(network)) {
+        this.currentNetwork = network;
+        const providerType = getProviderType(network);
+        const provider = this.web3Provider.getInstanceOf(providerType);
+        const networkId = getNetworkId(network) as SdkNetwork;
+        const sdkInstance = new Yearn(networkId, {
+          provider,
+        });
+        this.register(network, sdkInstance);
+
+        return sdkInstance;
+      }
       throw new Error(`YearnSdkImpl has no "${network}" network registered`);
     }
 
@@ -35,6 +45,7 @@ export class YearnSdkImpl implements YearnSdk {
   }
 
   public register(network: Network, instance: Yearn<SdkNetwork>): void {
-    this.instances.set(network, instance);
+    this.instance = instance;
+    this.currentNetwork = network;
   }
 }
