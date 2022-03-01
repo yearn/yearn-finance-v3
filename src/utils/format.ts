fc398daae8665fe20827d9120770926a95c0b408
Karelian Pie <devkarelianpie@gmail.com>
2022-03-01 14:59:53 +1100

    feat: Add support to `'error'` apyType to formatApy

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -96,6 +96,7 @@ export const formatUsd = (amount?: Amount, decimals = 2): FormattedAmount =>
   toBN(amount).toFormat(decimals, { ...FORMAT, prefix: '$ ' });
 
 export const formatApy = (apyData: Fraction, apyType: string): FormattedAmount => {
+  if (apyType === 'error') return '-';
   if (apyType === 'new') return 'NEW ✨';
   if (apyType === 'n/a') return 'N/A';
 
