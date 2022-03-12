2da8940e1c0a9388558819d93dedad524455797f
phillip055 <vorathep055@gmail.com>
2022-03-12 22:26:20 +0700

    feat: remove isLoading selector, move to another PR

--- src/core/store/modules/wallet/wallet.selectors.ts
+++ src/core/store/modules/wallet/wallet.selectors.ts
@@ -2,7 +2,6 @@ import { getNetwork } from '@utils';
 import { RootState } from '@types';
 
 const selectWallet = (state: RootState) => state.wallet;
-const selectWalletIsLoading = (state: RootState) => state.wallet.isLoading;
 const selectWalletIsConnected = (state: RootState) => state.wallet.isConnected;
 const selectSelectedAddress = (state: RootState) => state.wallet.selectedAddress;
 const selectAddressEnsName = (state: RootState) => state.wallet.addressEnsName;
@@ -11,7 +10,6 @@ const selectWalletNetwork = (state: RootState) =>
 
 export const WalletSelectors = {
   selectWallet,
-  selectWalletIsLoading,
   selectWalletIsConnected,
   selectSelectedAddress,
   selectAddressEnsName,
