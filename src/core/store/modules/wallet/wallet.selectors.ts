4a85adc359ec79716aa664c67fcd7bf864c14a3a
turtlemoji <78362532+turtlemoji@users.noreply.github.com>
2022-03-16 01:11:36 -0500

    fix: add selectAddress to selectWalletIsConnected selector

--- src/core/store/modules/wallet/wallet.selectors.ts
+++ src/core/store/modules/wallet/wallet.selectors.ts
@@ -2,7 +2,7 @@ import { getNetwork } from '@utils';
 import { RootState } from '@types';
 
 const selectWallet = (state: RootState) => state.wallet;
-const selectWalletIsConnected = (state: RootState) => state.wallet.isConnected;
+const selectWalletIsConnected = (state: RootState) => state.wallet.isConnected && !!state.wallet.selectedAddress;
 const selectSelectedAddress = (state: RootState) => state.wallet.selectedAddress;
 const selectAddressEnsName = (state: RootState) => state.wallet.addressEnsName;
 const selectWalletNetwork = (state: RootState) =>
