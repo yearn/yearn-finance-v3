ee17a64d506836ddac93678e0b4187200251ef00
Karelian Pie <devkarelianpie@gmail.com>
2022-03-26 10:26:00 +1100

    refactor: Use .toString() instead

--- src/utils/network.ts
+++ src/utils/network.ts
@@ -23,20 +23,20 @@ export const getNetworkId = (network: Network): number => {
 };
 
 export const getNetwork = (networkId: number): Network => {
-  switch (Number(networkId)) {
-    case 1:
+  switch (networkId.toString()) {
+    case '1':
       return 'mainnet';
-    case 2:
+    case '2':
       return 'morden';
-    case 3:
+    case '3':
       return 'ropsten';
-    case 4:
+    case '4':
       return 'rinkeby';
-    case 42:
+    case '42':
       return 'kovan';
-    case 250:
+    case '250':
       return 'fantom';
-    case 42161:
+    case '42161':
       return 'arbitrum';
     default:
       return 'other';
