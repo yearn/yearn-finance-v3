c0f3bd4de82e78711594ebace001a3768d978c81
turtlemoji <turtlemoji@protonmail.com>
2022-04-18 20:06:09 -0300

    build: update react-scripts

--- craco.config.js
+++ craco.config.js
@@ -1,6 +1,19 @@
 const CracoAlias = require('craco-alias');
 
 module.exports = {
+  webpack: {
+    configure: {
+      resolve: {
+        fallback: {
+          crypto: require.resolve('crypto-browserify'),
+          stream: require.resolve('stream-browserify'),
+          http: require.resolve('stream-http'),
+          https: require.resolve('https-browserify'),
+          os: require.resolve('os-browserify/browser'),
+        },
+      },
+    },
+  },
   plugins: [
     {
       plugin: CracoAlias,
