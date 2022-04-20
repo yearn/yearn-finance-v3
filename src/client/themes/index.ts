def2bc900cace68772db942ad62893ce29885526
turtlemoji <78362532+turtlemoji@users.noreply.github.com>
2022-04-20 00:53:55 -0300

    feat: add system prefs as a current theme [WEB-1469] (#603)
    * feat: add system prefs as a current theme
    
    * refactor: remove bad import

--- src/client/themes/index.ts
+++ src/client/themes/index.ts
@@ -20,8 +20,10 @@ export const getTheme = (theme?: Theme): DefaultTheme => {
       return classicTheme;
     case 'explorer':
       return explorerTheme;
-    default:
+    case 'system-prefs':
       const { matches: prefersColorSchemeDark } = window?.matchMedia('(prefers-color-scheme: dark)');
       return prefersColorSchemeDark ? darkTheme : lightTheme;
+    default:
+      return lightTheme;
   }
 };
