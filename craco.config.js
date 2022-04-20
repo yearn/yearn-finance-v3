5bbf894167a9668eb878a4a6752232fe9efdfea0
xgambitox <18649057+xgambitox@users.noreply.github.com>
2022-04-19 21:20:38 -0600

    refactor: initial update to cra v5

--- craco.config.js
+++ craco.config.js
@@ -1,9 +1,34 @@
-const CracoAlias = require('craco-alias');
+// TODO: Remove craco-alias
+// const CracoAlias = require('craco-alias');
+const { CracoAliasPlugin } = require('react-app-alias');
 
 module.exports = {
+  webpack: {
+    configure: (webpackConfig) => {
+      webpackConfig.resolve.fallback = {
+        crypto: require.resolve('crypto-browserify'),
+        stream: require.resolve('stream-browserify'),
+        http: require.resolve('stream-http'),
+        https: require.resolve('https-browserify'),
+        os: require.resolve('os-browserify/browser'),
+      };
+      return webpackConfig;
+    },
+    // configure: {
+    //   resolve: {
+    //     fallback: {
+    //       crypto: require.resolve('crypto-browserify'),
+    //       stream: require.resolve('stream-browserify'),
+    //       http: require.resolve('stream-http'),
+    //       https: require.resolve('https-browserify'),
+    //       os: require.resolve('os-browserify/browser'),
+    //     },
+    //   },
+    // },
+  },
   plugins: [
     {
-      plugin: CracoAlias,
+      plugin: CracoAliasPlugin, // TODO: verify options needed
       options: {
         source: 'tsconfig',
         baseUrl: './src',
