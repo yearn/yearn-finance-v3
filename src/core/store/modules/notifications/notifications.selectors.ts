b4d5e6cac135881122865eca8579a3a19108e6a4
turtlemoji <turtlemoji@protonmail.com>
2022-04-23 07:10:31 -0300

    fix: bad selector type

--- src/core/store/modules/notifications/notifications.selectors.ts
+++ src/core/store/modules/notifications/notifications.selectors.ts
@@ -5,7 +5,7 @@ import { RootState, Message } from '@types';
 /* ---------------------------------- State --------------------------------- */
 const selectDismissedMessageIds = (state: RootState) => state.notifications.dismissedMessageIds;
 const selectMessages = (state: RootState) => state.notifications.messages;
-const selectActiveMessages = createSelector<RootState, Message[], Number[], Message[]>(
+const selectActiveMessages = createSelector(
   [selectMessages, selectDismissedMessageIds],
   (messages, dismissedMessageIds) => {
     const activeMessages = messages.filter(({ id }) => !dismissedMessageIds.includes(id));
