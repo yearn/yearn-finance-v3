e0dc8b18202e9208916d4671c06bb4f34d83c68e
turtlemoji <turtlemoji@protonmail.com>
2022-04-23 06:37:17 -0300

    refactor: style, click, and icon

--- src/client/components/app/Transactions/components/TxError.tsx
+++ src/client/components/app/Transactions/components/TxError.tsx
@@ -1,11 +1,24 @@
 import { FC, useEffect, useState } from 'react';
 import styled from 'styled-components';
 
-import { ChevronDownIcon, ChevronUpIcon, ErrorIcon, Icon, Text, WarningIcon } from '@components/common';
+import { ChevronTxIcon, ErrorIcon, Icon, Text, WarningIcon } from '@components/common';
 import { useAppTranslation } from '@hooks';
 
+const StyledArrowIcon = styled(Icon)<{ isExpanded: boolean }>`
+  width: 1.6rem;
+  fill: currentColor;
+  flex-shrink: 0;
+  transform: rotate(180deg);
+  transition: transform 200ms ease-in-out;
+
+  ${({ isExpanded }) =>
+    isExpanded &&
+    `
+    transform: rotate(0);
+  `}
+`;
 const StyledIcon = styled(Icon)`
-  width: 2rem;
+  width: 2.4rem;
   fill: currentColor;
   flex-shrink: 0;
 `;
@@ -23,15 +36,16 @@ const StyledTextDescription = styled(Text)`
   color: inherit;
   max-width: 100%;
   font-size: 1.2rem;
+  user-select: text;
 `;
 
 const StyledTxError = styled.div<{ errorType?: ErrorType }>`
   display: flex;
   align-items: stretch;
   font-weight: 100;
-  font-size: 1.4rem;
+  font-size: 1.6rem;
   border-radius: ${({ theme }) => theme.globalRadius};
-  padding: 0.5rem 0.7rem;
+  padding: 0.8rem;
   flex-direction: column;
 
   background-color: ${({ theme }) => theme.colors.txModalColors.error.backgroundColor};
@@ -48,15 +62,24 @@ const StyledTxError = styled.div<{ errorType?: ErrorType }>`
 const StyledTxErrorTitle = styled.div`
   display: flex;
   flex: 1;
-  gap: 0.7rem;
+  gap: 0.8rem;
   align-items: center;
+
+  cursor: ${(props) => (props.onClick ? 'pointer' : 'default')};
 `;
 
 const StyledTxErrorDescription = styled.div<{ isExpanded: boolean; errorType?: ErrorType }>`
-  max-height: ${({ isExpanded }) => (isExpanded ? '7rem' : '0')};
+  max-height: 0;
   overflow-y: scroll;
-  margin-top: ${({ isExpanded }) => (isExpanded ? '1rem' : '0')};
-  transition: margin-top 0.5s ease-in-out, max-height 0.5s ease-in-out;
+  transition: margin 200ms ease-in-out, max-height 200ms ease-in-out;
+
+  ${({ isExpanded }) =>
+    isExpanded &&
+    `
+    max-height: 6.4rem;
+    margin: .8rem 0;
+  `}
+
   ::-webkit-scrollbar {
     width: 0.2rem;
   }
@@ -102,20 +125,20 @@ export const TxError: FC<TxErrorProps> = ({ errorTitle, errorDescription, errorT
     setIsExpanded(false);
   }, [errorTitle, errorDescription]);
 
+  const onExpandClick = () => {
+    setIsExpanded((prevIsExpanded) => !prevIsExpanded);
+  };
+
   return (
     <StyledTxError errorType={errorType} {...props}>
-      <StyledTxErrorTitle>
+      <StyledTxErrorTitle onClick={errorDescription ? onExpandClick : undefined}>
         <StyledIcon Component={errorType === 'warning' ? WarningIcon : ErrorIcon} />
 
         <StyledTextTitle>{errorTitle || t('errors.unknown')}</StyledTextTitle>
 
-        {errorDescription && (
-          <StyledIcon
-            onClick={() => setIsExpanded((prevIsExpanded) => !prevIsExpanded)}
-            Component={isExpanded ? ChevronUpIcon : ChevronDownIcon}
-          />
-        )}
+        {errorDescription && <StyledArrowIcon Component={ChevronTxIcon} isExpanded={isExpanded} />}
       </StyledTxErrorTitle>
+
       {errorDescription && (
         <StyledTxErrorDescription isExpanded={isExpanded} errorType={errorType}>
           <StyledTextDescription>{errorDescription}</StyledTextDescription>
--- src/client/components/common/Icon/index.tsx
+++ src/client/components/common/Icon/index.tsx
@@ -8,7 +8,7 @@ import { ReactComponent as CheckRoundIcon } from '@assets/icons/check-round.svg'
 import { ReactComponent as ChevronDownIcon } from '@assets/icons/chevron-down.svg';
 import { ReactComponent as ChevronLeftIcon } from '@assets/icons/chevron-left.svg';
 import { ReactComponent as ChevronRightIcon } from '@assets/icons/chevron-right.svg';
-import { ReactComponent as ChevronUpIcon } from '@assets/icons/chevron-up.svg';
+import { ReactComponent as ChevronTxIcon } from '@assets/icons/chevron-tx.svg';
 import { ReactComponent as CloseIcon } from '@assets/icons/close.svg';
 import { ReactComponent as CollapseIcon } from '@assets/icons/collapse.svg';
 import { ReactComponent as ConstructionIcon } from '@assets/icons/construction.svg';
@@ -86,7 +86,7 @@ export {
   ChevronDownIcon,
   ChevronRightIcon,
   ChevronLeftIcon,
-  ChevronUpIcon,
+  ChevronTxIcon,
   WalletMissingIcon,
   ConstructionIcon,
   EthereumIcon,
