3dc58914d0f48ceac0e001292d1a9e4772897198
xgambitox <18649057+xgambitox@users.noreply.github.com>
2022-04-25 22:41:23 -0600

    refactor: remove craco

---
+++ config-overrides.js
@@ -0,0 +1,23 @@
+const { aliasWebpack, aliasJest } = require('react-app-alias');
+
+const options = {
+  baseUrl: './src',
+};
+
+module.exports = {
+  webpack: function (config) {
+    const modifiedConfig = aliasWebpack(options)(config);
+    modifiedConfig.resolve.fallback = {
+      crypto: require.resolve('crypto-browserify'),
+      stream: require.resolve('stream-browserify'),
+      http: require.resolve('stream-http'),
+      https: require.resolve('https-browserify'),
+      os: require.resolve('os-browserify/browser'),
+    };
+    return modifiedConfig;
+  },
+  jest: function (config) {
+    const modifiedConfig = aliasJest(options)(config);
+    return modifiedConfig;
+  },
+};
