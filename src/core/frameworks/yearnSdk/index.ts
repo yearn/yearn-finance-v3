5c9e29ccf3b175ff8d50217226dda013842a3a73
xgambitox <18649057+xgambitox@users.noreply.github.com>
2022-05-03 23:24:00 -0600

    style: change const name

--- src/core/frameworks/yearnSdk/index.ts
+++ src/core/frameworks/yearnSdk/index.ts
@@ -16,7 +16,7 @@ export class YearnSdkImpl implements YearnSdk {
       const networkId = getNetworkId(network) as SdkNetwork;
       const sdkInstance = new Yearn(networkId, {
         provider,
-        partnerId: isLedger && networkId === 1 ? CONTRACT_ADDRESSES.LEDGER : undefined,
+        partnerId: isLedger && networkId === 1 ? CONTRACT_ADDRESSES.LEDGER_PARTNER_ID : undefined,
         ...(YEARN_SUBGRAPH_KEY && {
           subgraph: {
             mainnetSubgraphEndpoint: `https://gateway.thegraph.com/api/${YEARN_SUBGRAPH_KEY}/subgraphs/id/${YEARN_SUBGRAPH_ID}`,
