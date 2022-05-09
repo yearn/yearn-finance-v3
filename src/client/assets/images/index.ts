7cf3c5f695b058ec7a2a81f595a6b8d5fb9ccf55
pentacle <pentacle@pentacle.xyz>
2022-05-09 04:32:47 +0100

    feat: citadel banner [WEB-1495] (#637)
    * feat: Citadel banner [WEB-1495]
    
    * Update src/client/routes/Vaults/index.tsx
    
    Co-authored-by: Chef Karelian <78794805+karelianpie@users.noreply.github.com>
    
    * feat: Citadel banner [WEB-1495]
    
    * feat: Citadel banner [WEB-1495]
    
    * feat: Citadel banner [WEB-1495]
    
    * feat: Citadel banner [WEB-1495]
    
    * feat: Citadel banner [WEB-1495]
    
    * feat: fix yarn lock
    
    * fix: revert npmrc changes
    
    * style: add newline
    
    * refactor: locales and added locales
    
    Co-authored-by: Chef Karelian <78794805+karelianpie@users.noreply.github.com>
    Co-authored-by: xgambitox <18649057+xgambitox@users.noreply.github.com>
    Co-authored-by: turtlemoji <turtlemoji@protonmail.com>

--- src/client/assets/images/index.ts
+++ src/client/assets/images/index.ts
@@ -2,5 +2,6 @@ import AlchemyCertified from './alchemy-certified.svg';
 import YearnLogoFull from './yearn-logo-full.svg';
 import YearnLogo from './yearn-logo.svg';
 import MetamaskLogo from './metamask-logo.png';
+import GoblinTown from './goblin-town.jpg';
 
-export { AlchemyCertified, YearnLogoFull, YearnLogo, MetamaskLogo };
+export { AlchemyCertified, YearnLogoFull, YearnLogo, MetamaskLogo, GoblinTown };
