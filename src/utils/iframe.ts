cfb298100f27afac3d8467f91e3aae2928cf51b1
xgambitox <18649057+xgambitox@users.noreply.github.com>
2022-05-09 17:39:45 -0600

    refactor: iframe utils

---
+++ src/utils/iframe.ts
@@ -0,0 +1,21 @@
+export const isInIframe = () => {
+  try {
+    return window.self !== window.top;
+  } catch (e) {
+    return true;
+  }
+};
+
+export const isLoadedInOtherDomain = (domain: string) => {
+  return (
+    isInIframe() && (window?.location?.ancestorOrigins?.[0]?.includes(domain) || document.referrer.includes(domain))
+  );
+};
+
+export const isLedgerLive = () => {
+  return isLoadedInOtherDomain('ledger');
+};
+
+export const isGnosisApp = () => {
+  return isLoadedInOtherDomain('gnosis');
+};
