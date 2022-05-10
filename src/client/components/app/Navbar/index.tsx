54d470fa84f89cf7f41d7890e1f1fe68aae17170
Edwin Zhang <edwinzhang64@gmail.com>
2022-05-10 13:42:06 -0600

    feat: add breadcrumb to vaults for vault details (#647)

--- src/client/components/app/Navbar/index.tsx
+++ src/client/components/app/Navbar/index.tsx
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 import { ConnectWalletButton } from '@components/app';
-import { OptionList, EthereumIcon, FantomIcon, ArbitrumIcon } from '@components/common';
+import { OptionList, EthereumIcon, FantomIcon, ArbitrumIcon, Link } from '@components/common';
 import { useWindowDimensions } from '@hooks';
 import { Network } from '@types';
 import { device } from '@themes/default';
@@ -38,6 +38,11 @@ const StyledText = styled.h1<{ toneDown?: boolean }>`
     `
     color: ${theme.colors.texts};
   `}
+`;
+
+const StyledLink = styled(Link)`
+  font-size: 2.4rem;
+  font-weight: 700;
 `;
 
 const StyledNavbar = styled.header`
@@ -79,6 +84,7 @@ const getNetworkIcon = (network: Network) => {
 interface NavbarProps {
   className?: string;
   title?: string;
+  titleLink?: string;
   subTitle?: string;
   walletAddress?: string;
   addressEnsName?: string;
@@ -94,6 +100,7 @@ interface NavbarProps {
 export const Navbar = ({
   className,
   title,
+  titleLink,
   subTitle,
   walletAddress,
   addressEnsName,
@@ -131,7 +138,7 @@ export const Navbar = ({
     <StyledNavbar className={className}>
       {title && (
         <StyledText toneDown={secondTitleEnabled}>
-          {title}
+          {titleLink ? <StyledLink to={titleLink}>{title}</StyledLink> : title}
           {secondTitleEnabled && vaultText}
         </StyledText>
       )}
