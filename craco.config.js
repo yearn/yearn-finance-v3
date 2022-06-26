7ce51e46c69685c44e508d74be4b0bb789836888
xgambitox <18649057+xgambitox@users.noreply.github.com>
2022-06-26 00:13:54 -0600

    refactor: consistent env variables build

--- craco.config.js
+++ craco.config.js
@@ -1,4 +1,20 @@
 const CracoAlias = require('craco-alias');
+require('react-scripts/config/env');
+
+const REACT_APP = /^REACT_APP_/i;
+
+// NOTE: orders REACT_APP_ environment variables in alphabetical order for consistent build bundle
+const reactAppProcessEnv = Object.keys(process.env)
+  .filter((key) => REACT_APP.test(key))
+  .sort()
+  .reduce((env, key) => {
+    env[key] = process.env[key];
+    return env;
+  }, {});
+Object.keys(reactAppProcessEnv).forEach((key) => {
+  delete process.env[key];
+  process.env[key] = reactAppProcessEnv[key];
+});
 
 module.exports = {
   babel: {
