9e61de7ededc30b2ffe750363e2bd0192f434a02
xgambitox <18649057+xgambitox@users.noreply.github.com>
2022-07-13 21:36:56 -0600

    refactor: lint import

--- src/client/components/common/Dropdown/index.tsx
+++ src/client/components/common/Dropdown/index.tsx
@@ -1,4 +1,4 @@
-import { ElementType, FC, useState } from 'react';
+import { FC, useState } from 'react';
 import styled from 'styled-components';
 
 import { ChevronDownIcon, Icon } from '../Icon';
