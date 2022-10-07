f3d2cca702bbff0e115f7172a65a017eb87a7a3e
Daniel Constantin <hello@danielconstantin.net>
2022-10-07 15:04:06 +0300

    Fix selectWithdrawTokenOptionsByAsset

--- src/core/store/common/tokensByAsset.selectors.ts
+++ src/core/store/common/tokensByAsset.selectors.ts
@@ -1,5 +1,6 @@
 import { createSelector } from '@reduxjs/toolkit';
 import { memoize } from 'lodash';
+import { ZapOutWith } from '@yfi/sdk';
 
 import { toBN } from '@utils';
 import { getConfig } from '@config';
@@ -69,14 +70,17 @@ export const selectWithdrawTokenOptionsByAsset = createSelector(
       const { NETWORK_SETTINGS } = getConfig();
       const currentNetworkSettings = NETWORK_SETTINGS[currentNetwork];
       const zapsDisabled =
-        (!servicesEnabled.zaps && assetData.metadata.zapOutWith) || !currentNetworkSettings.zapsEnabled;
+        !servicesEnabled.zaps || !assetData.metadata.zapOutWith || !currentNetworkSettings.zapsEnabled;
       const mainVaultTokenAddress = zapsDisabled ? assetData.token : assetData.metadata.defaultDisplayToken;
       const withdrawTokenAddresses = [mainVaultTokenAddress];
       if (!zapsDisabled) {
         if (assetData.token !== mainVaultTokenAddress) withdrawTokenAddresses.push(assetData.token);
         withdrawTokenAddresses.push(
           ...Object.values(tokensMap)
-            .filter(({ address, supported }) => !withdrawTokenAddresses.includes(address) && supported.portalsZapOut)
+            .filter(
+              ({ address, supported }) =>
+                !withdrawTokenAddresses.includes(address) && supported[assetData.metadata.zapOutWith as ZapOutWith]
+            )
             .map(({ address }) => address)
         );
       }
