2bc2b3eb2a0859400621b096222e6d893cfdf1aa
xgambitox <18649057+xgambitox@users.noreply.github.com>
2022-10-17 11:07:22 -0600

    fix: early exit penalty

--- src/client/containers/VotingEscrowTabs/EarlyExitTab.tsx
+++ src/client/containers/VotingEscrowTabs/EarlyExitTab.tsx
@@ -1,19 +1,22 @@
-import { useAppSelector, useExecuteThunk } from '@hooks';
+import { useAppSelector, useExecuteThunk, useIsMounting } from '@hooks';
 import { VotingEscrowsActions, VotingEscrowsSelectors, WalletSelectors } from '@store';
 import { AmountInput } from '@components/app';
 import { Box, Text, Button } from '@components/common';
-import { humanize, toBN, weeksBetween } from '@utils';
+import { humanize, toBN, getTimeUntil, toWeeks, format } from '@utils';
 
 export const EarlyExitTab = () => {
+  const isMounting = useIsMounting();
   const [withdrawLocked, withdrawLockedStatus] = useExecuteThunk(VotingEscrowsActions.withdrawLocked);
   const isWalletConnected = useAppSelector(WalletSelectors.selectWalletIsConnected);
   const votingEscrow = useAppSelector(VotingEscrowsSelectors.selectSelectedVotingEscrow);
 
   const hasLockedAmount = !!votingEscrow?.earlyExitPenaltyRatio && toBN(votingEscrow?.DEPOSIT.userDeposited).gt(0);
-  const weeksToUnlock = votingEscrow?.unlockDate ? weeksBetween(new Date(), votingEscrow.unlockDate).toString() : '0';
+  const weeksToUnlock = votingEscrow?.unlockDate
+    ? toWeeks(getTimeUntil(votingEscrow.unlockDate.getTime())).toString()
+    : '0';
   const expectedTokens = hasLockedAmount
     ? toBN(votingEscrow?.DEPOSIT.userDeposited)
-        .times(votingEscrow?.earlyExitPenaltyRatio ?? 0)
+        .times(toBN(1).minus(votingEscrow?.earlyExitPenaltyRatio ?? 0))
         .toString()
     : '0';
 
@@ -36,7 +39,7 @@ export const EarlyExitTab = () => {
           <Box display="flex" gap="1.6rem">
             <AmountInput
               label="veYFI you have"
-              amount={humanize('amount', votingEscrow?.DEPOSIT.userBalance, votingEscrow?.decimals, 4)}
+              amount={humanize('amount', votingEscrow?.DEPOSIT.userBalance, votingEscrow?.decimals)}
               mt="1.6rem"
               width={1 / 2}
               disabled
@@ -45,16 +48,16 @@ export const EarlyExitTab = () => {
           </Box>
           <Box display="flex" alignItems="center" gap="1.6rem">
             <AmountInput
-              label="veYFI you get"
-              amount={humanize('amount', expectedTokens, votingEscrow?.token.decimals, 4)}
-              message={`Penalty: ${humanize('percent', votingEscrow?.earlyExitPenaltyRatio?.toString(), undefined, 0)}`}
+              label="YFI you get"
+              amount={humanize('amount', expectedTokens, votingEscrow?.token.decimals)}
+              message={`Penalty: ${format('percent', votingEscrow?.earlyExitPenaltyRatio?.toString(), 2)}`}
               mt="1.6rem"
               width={1 / 2}
               disabled
             />
             <Button
               onClick={executeWithdrawLocked}
-              disabled={!hasLockedAmount}
+              disabled={isMounting || !hasLockedAmount}
               filled
               width={1 / 2}
               height="5.6rem"
