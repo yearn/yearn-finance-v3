85d962287f6949328f88a097e9668bb561c4a1fd
sky cherfa <sherifcherfa@gmail.com>
2022-11-10 07:04:56 -0500

    fix text for spigot collateral on line page

--- src/client/components/app/LineDetailsDisplay/LineMetadata.tsx
+++ src/client/components/app/LineDetailsDisplay/LineMetadata.tsx
@@ -111,7 +111,9 @@ export const LineMetadata = (props: LineMetadataProps) => {
   const renderSpigotMetadata = () => {
     if (!revenue) return null;
     if (!totalRevenue)
-      return <MetricDisplay title={t('lineDetails:metadata.revenue.no-revenue')} data={`$ ${totalRevenue}`} />;
+      return (
+        <MetricDisplay title={t('lineDetails:metadata.revenue.no-revenue')} data={`$ ${totalRevenue} 0 / month`} />
+      );
     return <MetricDisplay title={t('lineDetails:metadata.revenue.per-month')} data={`$ ${totalRevenue}`} />;
   };
 
