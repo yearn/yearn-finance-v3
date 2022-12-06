73ca51bab6aead98bea08bab22b142234b573a67
xgambitox <18649057+xgambitox@users.noreply.github.com>
2022-12-05 23:28:03 -0600

    fix: meta config module export

--- meta.config.js
+++ meta.config.js
@@ -1,30 +1,31 @@
+const getMetaConfig = ({ title, description, url, image }) => {
+  return {
+    description: { name: 'description', content: description },
+    'theme-color': { name: 'theme-color', content: '#000000' },
+    'git-url': { name: 'git-url', content: 'https://github.com/yearn/yearn-finance-v3' },
+
+    'og:url': { property: 'og:url', content: url },
+    'og:type': { property: 'og:type', content: 'website' },
+    'og:title': { property: 'og:title', content: title },
+    'og:description': { property: 'og:description', content: description },
+    'og:image': { property: 'og:image', content: image },
+
+    'twitter:card': { name: 'twitter:card', content: 'summary_large_image' },
+    'twitter:site': { name: 'twitter:site', content: '@iearnfinance' },
+    'twitter:creator': { name: 'twitter:creator', content: '@iearnfinance' },
+    'twitter:title': { name: 'twitter:title', content: title },
+    'twitter:description': { name: 'twitter:description', content: description },
+    'twitter:image': { name: 'twitter:image', content: image },
+  };
+};
+
 module.exports = {
-  getMetaConfig: ({ title, description, url, image }) => {
-    return {
-      description: { name: 'description', content: description },
-      'theme-color': { name: 'theme-color', content: '#000000' },
-      'git-url': { name: 'git-url', content: 'https://github.com/yearn/yearn-finance-v3' },
-
-      'og:url': { property: 'og:url', content: url },
-      'og:type': { property: 'og:type', content: 'website' },
-      'og:title': { property: 'og:title', content: title },
-      'og:description': { property: 'og:description', content: description },
-      'og:image': { property: 'og:image', content: image },
-
-      'twitter:card': { name: 'twitter:card', content: 'summary_large_image' },
-      'twitter:site': { name: 'twitter:site', content: '@iearnfinance' },
-      'twitter:creator': { name: 'twitter:creator', content: '@iearnfinance' },
-      'twitter:title': { name: 'twitter:title', content: title },
-      'twitter:description': { name: 'twitter:description', content: description },
-      'twitter:image': { name: 'twitter:image', content: image },
-    };
-  },
   getHtmlConfig: (isVeYfiEnv) => {
     if (isVeYfiEnv) {
       const title = 'Yearn Finance | veYFI';
       return {
         title,
-        meta: this.getMetaConfig({
+        meta: getMetaConfig({
           title,
           description: 'YFI evolved',
           url: 'https://vote.yearn.finance',
@@ -36,7 +37,7 @@ module.exports = {
     const title = 'Yearn Finance';
     return {
       title,
-      meta: this.getMetaConfig({
+      meta: getMetaConfig({
         title,
         description: 'Put your digital assets to work and receive the best risk-adjusted yields in DeFi',
         url: 'https://yearn.finance',
